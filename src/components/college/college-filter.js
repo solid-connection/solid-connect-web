17476f588108075987ea9848a489ff0634c84c62
devMuromi <devmuromi@gmail.com>
2023-12-20 20:03:44 +0900

    feat: add filter design

--- src/components/college/college-filter.js
+++ src/components/college/college-filter.js
@@ -3,12 +3,14 @@ import classes from "./college-filter.module.css";
 function CollegeFilter() {
   return (
     <div className={classes.wrapper}>
-      <select className={classes.select}>
-        <option value="all">필터</option>
-        <option value="">필터2</option>
-        <option value="">필터3</option>
-        <option value="">필터4</option>
-      </select>
+      <div className={classes.selectWrapper}>
+        <select className={classes.select}>
+          <option value="all">필터</option>
+          <option value="">필터2</option>
+          <option value="">필터3</option>
+          <option value="">필터4</option>
+        </select>
+      </div>
     </div>
   );
 }
