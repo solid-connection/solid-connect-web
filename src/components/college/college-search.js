ef769b3987779db6bc519a7eac64eb678ec30024
devMuromi <devmuromi@gmail.com>
2023-12-21 13:47:55 +0900

    feat: add CollegeSearch logic

--- src/components/college/college-search.js
+++ src/components/college/college-search.js
@@ -1,21 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./college-search.module.css";
 
-function CollegeSearch() {
+function CollegeSearch(props) {
   const [country, setCountry] = useState("");
   const [region, setRegion] = useState("");
-  const [query, setQuery] = useState("");
+  const [text, setText] = useState("");
+  const [regionsOptions, setRegionsOptions] = useState([]);
+
+  const { countries } = props;
+
+  useEffect(() => {
+    setRegion("");
+    if (countries) {
+      const selectedCountry = countries.find((c) => c.name === country);
+      if (selectedCountry) {
+        setRegionsOptions(selectedCountry.regions);
+      } else {
+        setRegionsOptions([]);
+      }
+    }
+  }, [country, countries]);
+
+  function submitHandler(event) {
+    event.preventDefault();
+    props.onSearch(country, region, text);
+  }
 
   return (
     <div className={classes.search}>
-      <select className={classes.dropdown} value={country} onChange={(e) => setCountry(e.target.value)}>
-        <option value="">국가</option>
-      </select>
-      <select className={classes.dropdown} value={region} onChange={(e) => setRegion(e.target.value)}>
-        <option value="">지역</option>
-      </select>
-
-      <input type="text" placeholder="학교를 검색하세요." value={query} onChange={(e) => setQuery(e.target.value)} className={classes.searchInput} />
+      <form onSubmit={submitHandler}>
+        <select className={classes.dropdown} value={country} onChange={(e) => setCountry(e.target.value)}>
+          <option value="">국가</option>
+          {countries &&
+            countries.map((c) => (
+              <option key={c.id} value={c.name}>
+                {c.name}
+              </option>
+            ))}
+        </select>
+        <select className={classes.dropdown} value={region} onChange={(e) => setRegion(e.target.value)} disabled={!country}>
+          <option value="">지역</option>
+          {regionsOptions.map((r, index) => (
+            <option key={index} value={r}>
+              {r}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="학교를 검색하세요." value={text} onChange={(e) => setText(e.target.value)} className={classes.searchInput} />
+      </form>
     </div>
   );
 }
--- src/pages/college/index.js
+++ src/pages/college/index.js
@@ -1,34 +1,75 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import CollegeList from "../../components/college/college-list";
 import CollegeSearch from "@/components/college/college-search";
 import CollegeFilter from "@/components/college/college-filter";
 
-function CollegePage(props) {
-  const { colleges } = props;
+export default function CollegePage(props) {
+  const router = useRouter();
+  const { colleges, countries } = props;
 
   const [filteredColleges, setFilteredColleges] = useState(colleges);
 
+  useEffect(() => {
+    const { country, region, text } = router.query;
+
+    // const filtered = colleges.filter((college) => {
+    //   return (!country || college.country === country) && (!region || college.region === region) && (!text || college.name.includes(text));
+    // });
+
+    setFilteredColleges(colleges);
+  }, [router.query, colleges]);
+
+  function findCollegeHandler(country, region, text) {
+    let path = "/college";
+    if (country) {
+      path += `?country=${country}`;
+      if (region) {
+        path += `&region=${region}`;
+      }
+    }
+    if (text) {
+      if (!country) path += "?";
+      else path += "&";
+      path += `text=${text}`;
+    }
+    router.push(path);
+  }
+
   return (
     <Fragment>
-      <CollegeSearch />
+      <CollegeSearch countries={countries} onSearch={findCollegeHandler} />
       <CollegeFilter />
       <CollegeList colleges={filteredColleges} />
     </Fragment>
   );
 }
 
-export function getServerSideProps() {
+export function getServerSideProps(context) {
+  const { query } = context;
+  const { country, region, text } = query;
+  const colleges = getCollegeList(country, region, text);
+
+  const countries = [
+    { id: 1, name: "Canada", regions: ["Ontario", "Quebec", "British Columbia"] },
+    { id: 2, name: "United States", regions: ["New York", "California", "Texas"] },
+  ];
+
   return {
-    props: {
-      colleges: [
-        { uuid: 1, name: "보라스 대학교", country: "스웨덴", region: "예톄보리", requirements: "토익 775 토플 90" },
-        { uuid: 2, name: "보라스 대학교", country: "스웨덴", region: "예톄보리", requirements: "토익 775 토플 90" },
-      ],
-    },
+    props: { colleges: colleges, countries: countries },
   };
 }
 
-export default CollegePage;
+function getCollegeList(country, region, text) {
+  let colleges = [
+    { uuid: 1, name: "보라스 대학교", country: "스웨덴", region: "예톄보리", requirements: "토익 775 토플 90" },
+    { uuid: 2, name: "보라스 대학교", country: "스웨덴", region: "예톄보리", requirements: "토익 775 토플 90" },
+  ];
+  if (text) {
+    colleges.push({ uuid: 3, name: "보라스 대학교", country: "스웨덴", region: "예톄보리", requirements: "토익 775 토플 90" });
+  }
+  return colleges;
+}
 
 export const runtime = "experimental-edge";
