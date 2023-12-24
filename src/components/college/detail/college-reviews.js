20d0087e6c48a0baff88bbb7fa1f1f55197d656d
devMuromi <devmuromi@gmail.com>
2023-12-24 15:49:45 +0900

    feat: 마우스로 가로 스크롤 되게 변경

--- src/components/college/detail/college-reviews.js
+++ src/components/college/detail/college-reviews.js
@@ -1,10 +1,32 @@
+import React, { useRef, useEffect } from "react";
 import CollegeReviewCard from "./college-review-card";
-
 import styles from "./college-reviews.module.css";
 
 export default function CollegeReviews() {
+  const containerRef = useRef(null);
+
+  const handleWheel = (e) => {
+    if (containerRef.current) {
+      e.preventDefault(); // 기본 스크롤 동작 중지
+      containerRef.current.scrollLeft += e.deltaY;
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("wheel", handleWheel, { passive: false });
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div ref={containerRef} className={styles.container}>
       <div className={styles.items}>
         <CollegeReviewCard />
         <CollegeReviewCard />
