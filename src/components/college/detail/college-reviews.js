b750ce751c28e72981fe8f033deba87482e2db56
devMuromi <devmuromi@gmail.com>
2023-12-24 15:36:47 +0900

    feat: CollegeReviews 추가

--- src/components/college/detail/college-reviews.js
+++ src/components/college/detail/college-reviews.js
@@ -1,3 +1,20 @@
+import CollegeReviewCard from "./college-review-card";
+
+import styles from "./college-reviews.module.css";
+
 export default function CollegeReviews() {
-  return <div>리뷰들~</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.items}>
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+        <CollegeReviewCard />
+      </div>
+    </div>
+  );
 }
