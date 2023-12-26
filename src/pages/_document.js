df338dca43eca7110df4e16e42d0140fffff042e
devMuromi <devmuromi@gmail.com>
2023-12-26 16:53:33 +0900

    design: font 재적용

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <style>@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');</style>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@800&family=Roboto&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
