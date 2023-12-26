af7533a8bde56fe6a3b43763e473c3514784869c
devMuromi <devmuromi@gmail.com>
2023-12-26 13:55:39 +0900

    feat: 하단 네비게이션 경로에따른 색 적용

--- src/components/layout/bottom-navigation.js
+++ src/components/layout/bottom-navigation.js
@@ -1,40 +1,53 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 import FavoriteFilled from "../ui/FavoriteFilled";
-import classes from "./bottom-navigation.module.css";
+import styles from "./bottom-navigation.module.css";
 
 function BottomNavigation() {
+  const router = useRouter();
+
+  const specificRoutes = ["/college", "/community", "/mento", "/my"];
+  const isSpecificRouteActive = specificRoutes.some((specificRoute) => router.pathname.startsWith(specificRoute));
+
+  const navs = [
+    {
+      route: "/college",
+      text: "학교",
+      isActive: router.pathname.startsWith("/college"),
+    },
+    {
+      route: "/community",
+      text: "커뮤니티",
+      isActive: router.pathname.startsWith("/community"),
+    },
+    {
+      route: "/",
+      text: "홈",
+      isActive: !isSpecificRouteActive,
+    },
+    {
+      route: "/mento",
+      text: "멘토",
+      isActive: router.pathname.startsWith("/mento"),
+    },
+    {
+      route: "/my",
+      text: "마이",
+      isActive: router.pathname.startsWith("/my"),
+    },
+  ];
+
   return (
-    <nav className={classes.bottom_nav}>
-      <Link href="/college" className={classes.nav_item}>
-        <div className={classes.nav_icon}>
-          <FavoriteFilled />
-        </div>
-        <span className={classes.nav_text}>학교</span>
-      </Link>
-      <Link href="/community" className={classes.nav_item}>
-        <div className={classes.nav_icon}>
-          <FavoriteFilled />
-        </div>
-        <span className={classes.nav_text}>커뮤니티</span>
-      </Link>
-      <Link href="/" className={classes.nav_item}>
-        <div className={classes.nav_icon}>
-          <FavoriteFilled />
-        </div>
-        <span className={classes.nav_text}>홈</span>
-      </Link>
-      <Link href="/mento" className={classes.nav_item}>
-        <div className={classes.nav_icon}>
-          <FavoriteFilled />
-        </div>
-        <span className={classes.nav_text}>멘토</span>
-      </Link>
-      <Link href="/my" className={classes.nav_item}>
-        <div className={classes.nav_icon}>
-          <FavoriteFilled />
-        </div>
-        <span className={classes.nav_text}>마이</span>
-      </Link>
+    <nav className={styles.bottom_nav}>
+      {navs.map((nav, index) => (
+        <Link key={index} href={nav.route} className={styles.nav_item}>
+          <div className={styles.nav_icon}>
+            <FavoriteFilled color={nav.isActive ? "#1976D2" : "black"} />
+          </div>
+          <span className={nav.isActive ? styles.nav_text_active : styles.nav_text}>{nav.text}</span>
+        </Link>
+      ))}
     </nav>
   );
 }
