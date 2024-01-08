75be276571f68453b28e9d7df39433f6dc870917
devMuromi <devmuromi@gmail.com>
2024-01-08 20:12:09 +0900

    feat: 대학 정보 목록 페이지 필터링, 검색 추가

--- src/pages/college/index.js
+++ src/pages/college/index.js
@@ -10,24 +10,35 @@ import { getCollegeListData } from "../api/college";
 export default function CollegePage(props) {
   const router = useRouter();
   const { colleges, countries } = props;
-  const [searchText, setSearchText] = useState(router.query.query || "");
-  const filters = ["유럽권", "미주권", "아시아권"];
-  const [filter, setFilter] = useState({ country: "", region: "", text: "" });
 
+  const [searchText, setSearchText] = useState(router.query.query || "");
+  const filters = { 1: "유럽권", 2: "미주권", 3: "아시아권", 4: "중국권" };
+  const [filter, setFilter] = useState(0);
   const [filteredColleges, setFilteredColleges] = useState(colleges);
 
   useEffect(() => {
-    const { query } = router.query;
+    const filtered = colleges.filter((college) => {
+      const matchesRegion = filter ? college.region === filters[filter] : true;
+      const matchesSearchText = searchText ? college.name.toLowerCase().includes(searchText.toLowerCase()) : true;
+      return matchesRegion && matchesSearchText;
+    });
 
-    setFilteredColleges(colleges);
-  }, [router.query, colleges]);
+    setFilteredColleges(filtered);
+  }, [filter, searchText, colleges]);
 
-  function findCollegeHandler() {
-    let path = "/college";
-    if (searchText) {
-      path += `?query=${searchText}`;
-    }
-    router.push(path);
+  // useEffect(() => {
+  //   const { query } = router.query;
+
+  //   setFilteredColleges(colleges);
+  // }, [router.query, colleges]);
+
+  function findCollegeHandler(event) {
+    event.preventDefault();
+    // let path = "/college";
+    // if (searchText) {
+    //   path += `?query=${searchText}`;
+    // }
+    // router.push(path);
   }
 
   return (
@@ -43,14 +54,19 @@ export default function CollegePage(props) {
 export async function getServerSideProps(context) {
   const { query } = context;
   const { country, region, text } = query;
-  const colleges = await getCollegeList(country, region, text);
+  const colleges = await getCollegeList(region, text);
 
   return {
     props: { colleges: colleges },
   };
 }
 
-function getCollegeList(country, region, text) {
-  const colleges = getCollegeListData();
-  return colleges;
+async function getCollegeList(region, text) {
+  const colleges = await getCollegeListData();
+  const filteredColleges = colleges.filter((college) => {
+    const matchesRegion = region ? college.region === region : true;
+    const matchesSearchText = text ? college.name.toLowerCase().includes(text.toLowerCase()) : true;
+    return matchesRegion && matchesSearchText;
+  });
+  return filteredColleges;
 }
