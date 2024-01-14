52d56100908e3bb65e8310f1edc225c815b14eaf
devMuromi <devmuromi@gmail.com>
2024-01-14 20:13:22 +0900

    design: 하단 네비게이션 아이콘 수정

--- src/components/layout/bottom-navigation.js
+++ src/components/layout/bottom-navigation.js
@@ -3,6 +3,11 @@ import { useRouter } from "next/router";
 
 import FavoriteFilled from "../ui/FavoriteFilled";
 import styles from "./bottom-navigation.module.css";
+import DegreeHat from "./icon/DegreeHat";
+import EditTwo from "./icon/EditTwo";
+import Home from "./icon/Home";
+import WhatsNew from "./icon/WhatsNew";
+import Human from "./icon/Human";
 
 function BottomNavigation() {
   const router = useRouter();
@@ -15,26 +20,31 @@ function BottomNavigation() {
       route: "/college",
       text: "학교",
       isActive: router.pathname.startsWith("/college"),
+      icon: <DegreeHat />,
     },
     {
       route: "/community",
       text: "커뮤니티",
       isActive: router.pathname.startsWith("/community"),
+      icon: <EditTwo />,
     },
     {
       route: "/",
       text: "홈",
       isActive: !isSpecificRouteActive,
+      icon: <Home />,
     },
     {
       route: "/mento",
       text: "멘토",
       isActive: router.pathname.startsWith("/mento"),
+      icon: <WhatsNew />,
     },
     {
       route: "/my",
       text: "마이",
       isActive: router.pathname.startsWith("/my"),
+      icon: <Human />,
     },
   ];
 
@@ -43,7 +53,8 @@ function BottomNavigation() {
       {navs.map((nav, index) => (
         <Link key={index} href={nav.route} className={styles.nav_item}>
           <div className={styles.nav_icon}>
-            <FavoriteFilled color={nav.isActive ? "#1976D2" : "black"} />
+            {/* <FavoriteFilled color={nav.isActive ? "#1976D2" : "black"} /> */}
+            {nav.icon}
           </div>
           <span className={nav.isActive ? styles.nav_text_active : styles.nav_text}>{nav.text}</span>
         </Link>
