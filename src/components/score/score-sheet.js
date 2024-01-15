a0be0a85a5762273826e62a87fe73a2ef2d517d8
devMuromi <devmuromi@gmail.com>
2024-01-15 12:27:11 +0900

    design: score sheet design 수정

--- src/components/score/score-sheet.js
+++ src/components/score/score-sheet.js
@@ -1,27 +1,41 @@
+import { useState } from "react";
 import styles from "./score-sheet.module.css";
+import ExpendMoreFilled from "../ui/icon/ExpendMoreFilled";
 
 export default function ScoreSheet(props) {
   const { college, scores } = props;
+  const [tableOpened, setTableOpened] = useState(false);
   return (
     <table className={styles.table}>
       <thead>
         <tr>
-          <th>{college}</th>
-          <th>4.5</th>
-          <th>880</th>
-          <th>우선순위</th>
+          <th>
+            {college} ({3})
+          </th>
+          <th>
+            <button
+              onClick={() => {
+                setTableOpened(!tableOpened);
+              }}
+            >
+              <ExpendMoreFilled />
+            </button>
+          </th>
         </tr>
       </thead>
-      <tbody>
-        {scores.map((score) => (
-          <tr key={score.key}>
-            <td>{college}</td>
-            <td>{score.score}</td>
-            <td>{score.count}</td>
-            <td>우선순위</td>
-          </tr>
-        ))}
-      </tbody>
+      {tableOpened && (
+        <tbody>
+          {scores.map((score) => (
+            <tr key={score.key}>
+              <td>{score.key}</td>
+              <td>{score.name}</td>
+              <td>{score.score}</td>
+              <td>{score.languageType}</td>
+              <td>{score.languageScore}</td>
+            </tr>
+          ))}
+        </tbody>
+      )}
     </table>
   );
 }
