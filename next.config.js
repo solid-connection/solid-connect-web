06b493a6c20dd149193f18e64090845c8a150d12
devMuromi <devmuromi@gmail.com>
2024-01-20 02:29:49 +0900

    key: 키 수정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    GOOGLE_MAP_API_KEY: "AIzaSyA2dItMv0WEk0AKg4DgQipyuk8oR-DZgIg",
+    GOOGLE_MAP_API_KEY: "AIzaSyAm-HhRKVeHOF6uXmf6Mgw1yJPxwnlEf0w",
     KAKAO_CLIENT_ID: "b929e0134696bee489e7d9b9691161ba",
     KAKAO_CLIENT_SECRET: "9vuL2tIB5fSeYbjX6dEbFJFJTLjKndxJ",
   },
