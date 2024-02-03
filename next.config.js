022d6724c478888fff75ac517d9315776e9748a4
devMuromi <devmuromi@gmail.com>
2024-02-03 12:26:20 +0900

    remove: 일부 환경변수 삭제

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     GOOGLE_MAP_API_KEY: "AIzaSyAm-HhRKVeHOF6uXmf6Mgw1yJPxwnlEf0w",
-    // KAKAO_CLIENT_ID: "b929e0134696bee489e7d9b9691161ba",
     KAKAO_REDIRECT_URI: "http://localhost:3000/login/kakao",
-    // KAKAO_CLIENT_SECRET: "9vuL2tIB5fSeYbjX6dEbFJFJTLjKndxJ",
     KAKAO_JS_KEY: "b929e0134696bee489e7d9b9691161ba",
   },
   images: {
