abc0c01e8bca8d0e999925730b7c70c0b21ab727
devMuromi <devmuromi@gmail.com>
2024-02-05 19:24:29 +0900

    feat: 임시 배포 로그인 중지

--- src/pages/login/kakao/callback.js
+++ src/pages/login/kakao/callback.js
@@ -15,8 +15,8 @@ export default function KakaoLoginCallbackPage() {
     console.log("useEffect 실행됨");
 
     if (code) {
-      // console.log(code);
-      sendCodeToBackend(code);
+      console.log(code);
+      // sendCodeToBackend(code);
     }
   }, []);
 
