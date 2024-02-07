778797f50cbcda8797da955aef91055970b32044
devMuromi <devmuromi@gmail.com>
2024-02-07 21:58:18 +0900

    fix: inter font include 위치 변경

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <style>@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');</style>
         <meta name="description" content="solid connection" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1" />
         <link rel="icon" href="/favicon.svg" />
