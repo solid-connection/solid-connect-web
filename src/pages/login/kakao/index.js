77ee542642adf93aa6dfc5d7fd71d420e45b61b3
devMuromi <devmuromi@gmail.com>
2024-02-08 15:18:05 +0900

    fix: 모바일에서 카카오톡으로 넘어가는 오류 수정

--- src/pages/login/kakao/index.js
+++ src/pages/login/kakao/index.js
@@ -3,7 +3,6 @@ import KakaoLoginButton from "@/components/login/kakao-login-button";
 export default function KakaoLoginPage() {
   function kakaoLogin() {
     window.Kakao.Auth.authorize({
-      // redirectUri: "http://localhost:3000/login/kakao/callback",
       redirectUri: `${process.env.NEXT_PUBLIC_WEB_URL}/login/kakao/callback`,
     });
   }
