1a406a116c994937f073402c78a3a2d9096c0bd0
devMuromi <devmuromi@gmail.com>
2024-02-09 23:54:21 +0900

    design: 홈 화면 추천 파견학교 이미지 그라데이션 추가

--- src/components/home/home-college-card.js
+++ src/components/home/home-college-card.js
@@ -8,7 +8,9 @@ export default function HomeCollegeCard(props) {
   return (
     <Link href={`/college/${id}`}>
       <div className={styles.card}>
-        <Image className={styles.image} src={image} width={153} height={120} alt={name || "대학 없음"} />
+        <div className={styles.imageWrapper}>
+          <Image className={styles.image} src={image} width={153} height={120} objectFit="cover" alt={name || "대학 없음"} />
+        </div>
         <div className={styles.name}>{name}</div>
       </div>
     </Link>
