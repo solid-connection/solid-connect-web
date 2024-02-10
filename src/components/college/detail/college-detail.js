5aad444d70b926528bfbeea30e5bbcc3f5c09d52
devMuromi <devmuromi@gmail.com>
2024-02-10 10:50:28 +0900

    desgin: 대학 세부화면 배경화면 그라데이션 추가

--- src/components/college/detail/college-detail.js
+++ src/components/college/detail/college-detail.js
@@ -6,7 +6,9 @@ export default function CollegeDetail(props) {
   const { image, name } = props;
   return (
     <div className={styles.wrapper}>
-      <Image className={styles.image} src={image} alt={name} width={600} height={300} />
+      <div className={styles.imageWrapper}>
+        <Image className={styles.image} src={image} alt={name} width={600} height={300} />
+      </div>
     </div>
   );
 }
