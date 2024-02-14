f3b42c9cbf09578ba655fd7cf5dc13b9dcf3aba9
devMuromi <devmuromi@gmail.com>
2024-02-14 22:19:08 +0900

    feat: 홈화면 추천 파견학교 로그인시 맞춤 파견 되게 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import createApiClient from "@/lib/serverApiClient";
 
 import TopNavigation from "@/components/layout/top-navigation";
 import Home from "@/components/home/home";
@@ -20,8 +21,17 @@ export default function HomePage(props) {
 }
 
 export async function getServerSideProps(context) {
-  const data = await getRecommendedCollegesData();
-  const recommendedColleges = data.recommendedUniversities;
+  const { req, res } = context;
+  let recommendedColleges = [];
+  try {
+    const apiClient = createApiClient(req, res);
+    const reponse = await apiClient.get("/home");
+    recommendedColleges = reponse.data.data.recommendedUniversities;
+  } catch (error) {
+    const data = await getRecommendedCollegesData();
+    recommendedColleges = data.recommendedUniversities;
+  }
+
   const newsList = [{ id: 1, imageUrl: "/news/1.jpeg", url: "https://blog.naver.com/yoon144950/223349958663", title: "교환학생 해외 대학 학점 인정은 어떻게 받아요?" }];
   return {
     props: { recommendedColleges: recommendedColleges, newsList: newsList },
