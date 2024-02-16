afb4aa3d58239cecb502bcb6a96fdf41797e0375
devMuromi <devmuromi@gmail.com>
2024-02-17 01:17:32 +0900

    remove: 미사용 이미지 제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export async function getServerSideProps(context) {
     recommendedColleges = data.recommendedUniversities;
   }
 
-  const newsList = [{ id: 1, imageUrl: "/news/1.jpeg", url: "https://blog.naver.com/yoon144950/223349958663", title: "교환학생 해외 대학 학점 인정은 어떻게 받아요?" }];
+  const newsList = [{ id: 1, imageUrl: "/images/news/1.jpeg", url: "https://blog.naver.com/yoon144950/223349958663", title: "교환학생 해외 대학 학점 인정은 어떻게 받아요?" }];
   return {
     props: { recommendedColleges: recommendedColleges, newsList: newsList },
   };
