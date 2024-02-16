505a910dbab9bc28f86ef9adc6362dc73f3bb81d
devMuromi <devmuromi@gmail.com>
2024-02-16 23:44:56 +0900

    refactor: CollegeCards, CollegeCard 파일 통합

--- src/components/college/list/college-cards.js
+++ src/components/college/list/college-cards.js
@@ -1,5 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import styles from "./college-cards.module.css";
-import CollegeCard from "./college-card";
+import CheveronRightFilled from "../../ui/icon/ChevronRightFilled";
 
 export default function CollegeCards(props) {
   const { colleges } = props;
@@ -9,5 +12,41 @@ export default function CollegeCards(props) {
         <CollegeCard key={college.id} {...college} />
       ))}
     </div>
+  );
+}
+
+export function CollegeCard(props) {
+  const { id, koreanName, region, country, logoImageUrl, studentCapacity, languageRequirements } = props;
+  return (
+    <Link className={styles.card} href={`/college/${id}`}>
+      <div className={styles.centerAlign}>
+        <Image className={styles.image} src={logoImageUrl} width={100} height={100} alt={koreanName || "이미지 없음"} />
+      </div>
+
+      <div className={styles.info}>
+        <span className={styles.name}>{koreanName}</span>
+        <div className={styles.spaceBetween}>
+          <span className={styles.country}>
+            {country} | {region}
+          </span>
+          <span className={styles.capacity}>모집 {studentCapacity}명</span>
+        </div>
+        <div className={styles.requirements}>
+          {languageRequirements
+            .map((requirement, index) => {
+              return (
+                <span key={index} className={styles.requirement}>
+                  {requirement.languageTestType.replace(/_/g, " ")}: {requirement.minScore}
+                </span>
+              );
+            })
+            .slice(0, 3)}
+        </div>
+      </div>
+
+      <div className={styles.rightArrowIcon}>
+        <CheveronRightFilled color="black" opacity="0.54" />
+      </div>
+    </Link>
   );
 }
