65d399bf8bb1a03d5985177ce15e7144df6f5aec
devMuromi <devmuromi@gmail.com>
2024-02-17 00:14:25 +0900

    fix: 사파리에서 카카오 로그인 버튼 로딩 안되는 문제 수정

--- src/components/login/kakao-login-button.js
+++ src/components/login/kakao-login-button.js
@@ -3,5 +3,5 @@ import styles from "./kakao-login-button.module.css";
 
 export default function KakaoLoginButton(props) {
   const { onClick } = props;
-  return <Image style={props.style} className={styles.image} src="/images/kakao_login_large_wide.png" alt="카카오 로그인" width={600} height={90} onClick={onClick} />;
+  return <Image style={props.style} className={styles.image} src="/images/kakao_login_large_wide.png" alt="카카오 로그인" width={599} height={90} onClick={onClick} />;
 }
