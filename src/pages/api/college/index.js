93262e4e787a0c7c427c24d98fbcd270d4b17973
devMuromi <devmuromi@gmail.com>
2024-02-18 05:07:53 +0900

    feat: college list page api연결로 수정

--- src/pages/api/college/index.js
+++ src/pages/api/college/index.js
@@ -2,35 +2,35 @@ import path from "path";
 import fs from "fs";
 
 export async function getCollegeListData() {
-  // try {
-  //   const backendResponse = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/university/search`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  //   // 오류 처리
-  //   if (!backendResponse.ok) {
-  //     const errorData = await backendResponse.json();
-  //     throw new Error(errorData.message);
-  //   }
-  //   const data = await backendResponse.json();
-  //   if (!data.success) {
-  //     throw new Error(data.error);
-  //   }
+  try {
+    const backendResponse = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/university/search`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // 오류 처리
+    if (!backendResponse.ok) {
+      const errorData = await backendResponse.json();
+      throw new Error(errorData.message);
+    }
+    const data = await backendResponse.json();
+    if (!data.success) {
+      throw new Error(data.error);
+    }
 
-  //   return data;
-  // } catch (error) {
-  //   console.error(error);
-  //   return { error: error.message };
-  // }
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
 
   // 파일에서 불러오기
-  const filePath = path.join(process.cwd(), "datas/24-2colleges.json");
-  const fileData = fs.readFileSync(filePath);
-  const collegeData = JSON.parse(fileData);
+  // const filePath = path.join(process.cwd(), "datas/24-2colleges.json");
+  // const fileData = fs.readFileSync(filePath);
+  // const collegeData = JSON.parse(fileData);
 
-  return { data: collegeData };
+  // return { data: collegeData };
 }
 
 export default async function handler(req, res) {
