396033d0d26e72ded33c1d48e17bcb737ed2f582
devMuromi <devmuromi@gmail.com>
2024-02-18 06:24:48 +0900

    fix, refactor: 홈화면 homeCollegeCards 수정

--- src/components/home/home-college-cards.js
+++ src/components/home/home-college-cards.js
@@ -1,16 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
 import styles from "./home-college-cards.module.css";
-import HomeCollegeCard from "./home-college-card";
 
 export default function HomeCollegeCards(props) {
-  function modifyUrl(url) {
-    // 'original'을 'resize'로 바꾸기
-    let newUrl = url.replace("original", "resize");
-    // 파일 확장자를 '.webp'로 바꾸기
-    newUrl = newUrl.replace(/\.\w+$/, ".webp");
-    return newUrl;
-  }
-
   const { colleges } = props;
 
   const containerRef = useRef(null);
@@ -38,9 +32,23 @@ export default function HomeCollegeCards(props) {
     <div ref={containerRef} className={styles.container}>
       <div className={styles.items}>
         {colleges.map((college) => (
-          <HomeCollegeCard key={college.id} id={college.id} image={college.backgroundImgUrl ? modifyUrl(college.backgroundImgUrl) : ""} name={college.koreanName || "대학명"} />
+          <HomeCollegeCard key={college.id} id={college.id} image={college.backgroundImgUrl ? college.backgroundImgUrl : ""} name={college.koreanName || "대학명"} />
         ))}
       </div>
     </div>
+  );
+}
+
+export function HomeCollegeCard(props) {
+  const { id, image, name } = props;
+  return (
+    <Link href={`/college/${id}`}>
+      <div className={styles.card}>
+        <div className={styles.imageWrapper}>
+          <Image src={image} width={153} height={120} alt={name || "대학 없음"} />
+        </div>
+        <div className={styles.name}>{name}</div>
+      </div>
+    </Link>
   );
 }
