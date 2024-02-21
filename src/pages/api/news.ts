a2e3219aabaedd953beee168708ef520f3f3ffa1
devMuromi <devmuromi@gmail.com>
2024-02-21 23:00:36 +0900

    feat: 소식지 데이터 추가 5, 6

--- src/pages/api/news.ts
+++ src/pages/api/news.ts
@@ -7,6 +7,8 @@ interface News {
 
 export async function getNewsList() {
   const newsList: News[] = [
+    { id: 6, title: "독일에 대한 기초 지식", imageUrl: "/images/news/6.png", url: "https://blog.naver.com/yoon144950/223359192982" },
+    { id: 5, title: "교환학생 합격, 수학계획서는 정말 안 중요할까요?", imageUrl: "/images/news/5.png", url: "https://blog.naver.com/yoon144950/223359719803" },
     { id: 4, title: "일리노이 대학교 공과대학 B형 정현수님 인터뷰", imageUrl: "/images/news/4.png", url: "https://blog.naver.com/yoon144950/223357293186" },
     { id: 3, title: "[스웨덴 교환학생] 인하대 교환학생 출국준비 Time line", imageUrl: "/images/news/3.png", url: "https://blog.naver.com/a996600/223265965232" },
     { id: 2, title: "교환학생 회고록", imageUrl: "/images/news/2.png", url: "https://yonsodev.tistory.com/m/90" },
