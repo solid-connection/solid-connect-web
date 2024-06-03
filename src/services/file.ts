ec5e7128cc9dda8fff9590e8cccc7525c10677d8
devMuromi <devmuromi@gmail.com>
2024-06-03 10:10:00 +0900

    feat: File service 추가

---
+++ src/services/file.ts
@@ -0,0 +1,13 @@
+import apiClient from "@/lib/axiosClient";
+import noAuthAxios from "@/lib/noAuthAxiosClient";
+import { AxiosResponse } from "axios";
+import { ApiResponse } from "@/types/response";
+import { FileResponse } from "@/types/file";
+
+export const uploadProfileImageFileUnauthorizedApi = (file: File): Promise<AxiosResponse<ApiResponse<FileResponse>>> => noAuthAxios.post("/file/profile/pre", { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
+
+export const uplaodProfileImageFileApi = (file: File): Promise<AxiosResponse<ApiResponse<FileResponse>>> => apiClient.post("/file/profile/post", { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
+
+export const uploadGpaFileApi = (file: File): Promise<AxiosResponse<ApiResponse<FileResponse>>> => apiClient.post("/file/gpa", { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
+
+export const uploadLanguageTestFileApi = (file: File): Promise<AxiosResponse<ApiResponse<FileResponse>>> => apiClient.post("/file/language-test", { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
