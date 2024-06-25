368287ea969f277c3173e637187da7b4d6f64e63
devMuromi <devmuromi@gmail.com>
2024-06-26 01:02:37 +0900

    refactor: firebase 호출 libs에 분리

---
+++ src/libs/keywords.ts
@@ -0,0 +1,29 @@
+import initializeFirebaseAdmin from "./firebaseAdmin";
+import admin from "firebase-admin";
+
+export async function fetchPopularKeywords(): Promise<string[]> {
+  const db = await initializeFirebaseAdmin();
+
+  const snapshot = await db.collection("keywords").orderBy("count", "desc").limit(10).get();
+  const keywords = snapshot.docs.map((doc) => doc.id);
+  return keywords;
+}
+
+export async function saveSearchKeyword(keyword: string) {
+  const db = await initializeFirebaseAdmin();
+
+  const keywordRef = db.collection("keywords").doc(keyword);
+  const docSnap = await keywordRef.get();
+
+  if (docSnap.exists) {
+    await keywordRef.update({
+      count: admin.firestore.FieldValue.increment(1),
+      lastSearched: new Date(),
+    });
+  } else {
+    await keywordRef.set({
+      count: 1,
+      lastSearched: new Date(),
+    });
+  }
+}
---
+++ src/pages/api/keywords.ts
@@ -0,0 +1,25 @@
+import { fetchPopularKeywords, saveSearchKeyword } from "@/libs/keywords";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // 검색어 저장
+    const { keyword } = req.body;
+    try {
+      saveSearchKeyword(keyword);
+      res.status(200).json({ message: "Keyword updated successfully" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else if (req.method === "GET") {
+    // 인기 검색어 조회
+    try {
+      const keywords = await fetchPopularKeywords();
+      res.status(200).json(keywords);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
