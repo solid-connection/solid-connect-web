5a6f1a956625d2838770769108d3bba9e7582426
devMuromi <devmuromi@gmail.com>
2024-07-05 02:10:10 +0900

    feat: accessToken reissue API endpoint service reissueAccessTokenApi() 추가

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -12,4 +12,29 @@ export const signOutApi = (): Promise<AxiosResponse<ApiResponse<null>>> => apiCl
 
 export const deleteAccountApi = (): Promise<AxiosResponse<ApiResponse<null>>> => apiClient.patch("/auth/quit");
 
-export const reissueAccessTokenApi = () => {};
+export const reissueAccessTokenApi = async (refreshToken: string): Promise<string> => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/auth/reissue`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    if (data.success && data.data && data.data.accessToken) {
+      return data.data.accessToken;
+    } else {
+      throw new Error("Access token not found in the response");
+    }
+  } catch (error) {
+    console.error("Error reissuing access token:", error);
+    throw error;
+  }
+};
