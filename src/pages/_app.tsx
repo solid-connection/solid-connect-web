d3b82b975f0f66a0207e36df4152051850f0b3b0
devMuromi <devmuromi@gmail.com>
2024-07-04 21:45:26 +0900

    feat: RecoilRoot 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Script from "next/script";
 import "@/styles/globals.css";
 import { LayoutProvider } from "@/context/LayoutContext";
 import Layout from "@/components/layout/layout";
+import { RecoilRoot } from "recoil";
 
 // fonts
 import { Inter } from "next/font/google";
@@ -24,24 +25,26 @@ export default function App({ Component, pageProps }) {
     // console.log(window.Kakao.isInitialized());
   }
   return (
-    <LayoutProvider>
-      <>
-        <Head>
-          <meta name="description" content="solid connection" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1" />
-          <meta property="og:title" content="솔리드 커넥션" />
-          <meta property="og:description" content="솔리드 커넥션. 교환학생의 첫 걸음" />
-          <meta property="og:image" content="/images/site-thumbnail.png" />
-          <meta property="og:url" content="https://solid-connect.net" />
-          <meta property="og:locale" content="ko_KR" />
-          <link rel="icon" href="/icons/favicon_48.ico" />
-          <title>솔리드 커넥션</title>
-        </Head>
-        <Layout>
-          <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit} />
-          <Component {...pageProps} />
-        </Layout>
-      </>
-    </LayoutProvider>
+    <RecoilRoot>
+      <LayoutProvider>
+        <>
+          <Head>
+            <meta name="description" content="solid connection" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1" />
+            <meta property="og:title" content="솔리드 커넥션" />
+            <meta property="og:description" content="솔리드 커넥션. 교환학생의 첫 걸음" />
+            <meta property="og:image" content="/images/site-thumbnail.png" />
+            <meta property="og:url" content="https://solid-connect.net" />
+            <meta property="og:locale" content="ko_KR" />
+            <link rel="icon" href="/icons/favicon_48.ico" />
+            <title>솔리드 커넥션</title>
+          </Head>
+          <Layout>
+            <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit} />
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </LayoutProvider>
+    </RecoilRoot>
   );
 }
