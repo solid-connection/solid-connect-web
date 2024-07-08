8051737ed01830e4025149f5ab74767ccfe21118
devMuromi <devmuromi@gmail.com>
2024-07-08 21:29:00 +0900

    fix: 구글 애널리틱스 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,11 +37,12 @@ export default function App({ Component, pageProps }) {
           <meta property="og:locale" content="ko_KR" />
           <link rel="icon" href="/icons/favicon_48.ico" />
           <title>솔리드 커넥션</title>
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
         </Head>
         <Layout>
           <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit} />
           <Component {...pageProps} />
+          {/* <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} /> */}
+          <GoogleAnalytics gaId="G-V1KLYZC1DS" />
         </Layout>
       </>
     </LayoutProvider>
