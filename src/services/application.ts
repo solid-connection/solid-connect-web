e934f3c6d7cfad913a99e933fbe35b767a54f777
devMuromi <devmuromi@gmail.com>
2024-07-15 01:32:01 +0900

    refactor: API 엔드포인트 변경, response 타입 일괄 변경

--- src/services/application.ts
+++ src/services/application.ts
@@ -1,20 +1,19 @@
 import { axiosInstance } from "@/utils/axiosInstance";
 import { AxiosResponse } from "axios";
-import { ApiResponse } from "@/types/response";
 import { ApplicationListResponse, ApplicationScoreRequest, ApplicationStatusResponse, ApplicationUniversityRequest } from "@/types/application";
 
-export const postApplicationScoreApi = (applicationScoreRequest: ApplicationScoreRequest): Promise<AxiosResponse<ApiResponse<null>>> => {
+export const postApplicationScoreApi = (applicationScoreRequest: ApplicationScoreRequest): Promise<AxiosResponse<null>> => {
   return axiosInstance.post("/application/score", applicationScoreRequest);
 };
 
-export const postApplicationUniversityApi = (applicationUniversityRequest: ApplicationUniversityRequest): Promise<AxiosResponse<ApiResponse<null>>> => {
+export const postApplicationUniversityApi = (applicationUniversityRequest: ApplicationUniversityRequest): Promise<AxiosResponse<null>> => {
   return axiosInstance.post("/application/university", applicationUniversityRequest);
 };
 
-export const getApplicationListApi = (): Promise<AxiosResponse<ApiResponse<ApplicationListResponse>>> => {
+export const getApplicationListApi = (): Promise<AxiosResponse<ApplicationListResponse>> => {
   return axiosInstance.get("/application");
 };
 
-export const getMyApplicationStatusApi = (): Promise<AxiosResponse<ApiResponse<ApplicationStatusResponse>>> => {
+export const getMyApplicationStatusApi = (): Promise<AxiosResponse<ApplicationStatusResponse>> => {
   return axiosInstance.get("/application/status");
 };
--- src/types/response.ts
+++
@@ -1,6 +0,0 @@
-export interface ApiError {
-  code: string;
-  message: string;
-}
-
-export type ApiResponse<T> = { success: true; data: T } | { success: false; error: ApiError };
