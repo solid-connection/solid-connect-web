dde8f926a40a3d1f91d8242d9805fd1a60fccf49
devMuromi <devmuromi@gmail.com>
2024-07-23 05:04:09 +0900

    feat: 커뮤니티 권역 설정 selector 컴포넌트 추가

---
+++ src/containers/community/community-region-selector.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import CheckBoxOutlineBlankOutlined from "@/components/ui/icon/CheckBoxOutlineBlankOutlined";
+import ExpendMoreFilled from "@/components/ui/icon/ExpendMoreFilled";
+import Modal from "@/components/ui/modal";
+
+import styles from "./community-region-selector.module.css";
+
+export default function CommunityRegionSelector({ curRegion, setCurRegion, regionChoices }) {
+  const [isRegionSelectorModalVisible, setIsRegionSelectorModalVisible] = useState<boolean>(false);
+  const toggleRegionSelectorModal = () => {
+    setIsRegionSelectorModalVisible((prev) => !prev);
+  };
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.title} onClick={toggleRegionSelectorModal}>
+          {curRegion}
+        </div>
+        <button className={styles.button} onClick={toggleRegionSelectorModal}>
+          <ExpendMoreFilled />
+        </button>
+      </div>
+      <Modal isOpen={isRegionSelectorModalVisible} onClose={toggleRegionSelectorModal}>
+        <div className={styles.modal}>
+          <div className={styles.modal__header}>권역 변경</div>
+          <div className={styles.modal__category}>
+            {regionChoices.map((region) => (
+              <div
+                className={styles.modal__element_wrapper}
+                key={region}
+                onClick={() => {
+                  setCurRegion(region);
+                  toggleRegionSelectorModal();
+                }}
+              >
+                <CheckBoxOutlineBlankOutlined />
+                <div className={styles.modal__element}>{region}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+}
