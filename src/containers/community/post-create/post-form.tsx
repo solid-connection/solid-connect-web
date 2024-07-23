e722d1d3251dbe8a9cb1a3da6b6cdb53c1ad4146
devMuromi <devmuromi@gmail.com>
2024-07-23 17:18:37 +0900

    feat: 커뮤니티 글 작성 ui 추가

--- src/containers/community/post-create/post-form.tsx
+++ src/containers/community/post-create/post-form.tsx
@@ -1,15 +1,39 @@
+import { useEffect, useRef } from "react";
+
+import CheckBoxOutlineBlankOutlined from "@/components/ui/icon/CheckBoxOutlineBlankOutlined";
+
 import styles from "./post-form.module.css";
 
 export default function PostForm(props) {
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      const adjustHeight = () => {
+        textarea.style.height = "auto";
+        textarea.style.height = `${Math.min(textarea.scrollHeight, 100)}px`;
+      };
+
+      textarea.addEventListener("input", adjustHeight);
+      return () => textarea.removeEventListener("input", adjustHeight);
+    }
+  }, []);
+
   const notice =
-    "글 작성 시 유의사항!\n\n자유 게시판에서는 교환학생 기간 중 있었던 에피소드와 꿀팁들을 함께 공유할 수 있습니다.\n\n\n인종, 성별, 출신, 지역, 이념 관련 차별 관련 발언은 자동 삭제 조치됩니다. 타인에게 불쾌감을 유발할 수 있는 글은 삭제됩니다.\n솔리드 커넥션은 홍보 행위를 철저히 금지합니다.";
+    "글 작성 시 유의사항\n\n인종, 성별, 출신, 지역, 이념 관련 차별 관련 발언 및 타인에게 불쾌감을 유발할 수 있는 글은 삭제될 수 있습니다.\n\n솔리드 커넥션은 홍보 행위를 철저히 금지합니다.";
   return (
     <div className={styles.form}>
       <div className={styles.title}>
-        <input type="text" placeholder="제목을 입력하세요" />
+        <textarea placeholder="제목을 입력하세요" maxLength={40} rows={2}></textarea>
       </div>
-      <div className={styles.inputs}>
-        <></>
+      <div className={styles.question}>
+        <div className={styles.question__select}>
+          <div className={styles.question__box}>
+            <CheckBoxOutlineBlankOutlined size={18} />
+          </div>
+          질문으로 업로드 하기
+        </div>
       </div>
       <div className={styles.content}>
         <textarea placeholder="내용을 입력하세요" />
