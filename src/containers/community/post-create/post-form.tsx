a6a4e17a1e4a0980f382ffb4aacd6bff208d53b8
devMuromi <devmuromi@gmail.com>
2024-08-02 13:21:43 +0900

    feat: 글 작성 ui 수정

--- src/containers/community/post-create/post-form.tsx
+++ src/containers/community/post-create/post-form.tsx
@@ -6,26 +6,46 @@ import styles from "./post-form.module.css";
 
 export default function PostForm(props) {
   const textareaRef = useRef(null);
+  const titleRef = useRef(null);
 
   useEffect(() => {
     const textarea = textareaRef.current;
-    if (textarea) {
-      const adjustHeight = () => {
-        textarea.style.height = "auto";
-        textarea.style.height = `${Math.min(textarea.scrollHeight, 100)}px`;
-      };
+    const title = titleRef.current;
 
-      textarea.addEventListener("input", adjustHeight);
-      return () => textarea.removeEventListener("input", adjustHeight);
-    }
+    const adjustHeight = () => {
+      textarea.style.height = "auto";
+      const scrollHeight = textarea.scrollHeight;
+      const newHeight = scrollHeight <= 50 ? 50 : Math.min(scrollHeight, 100);
+      textarea.style.height = `${newHeight}px`;
+      title.style.height = `${newHeight}px`;
+    };
+
+    textarea.addEventListener("input", adjustHeight);
+
+    // 초기 높이 설정
+    adjustHeight();
+
+    return () => textarea.removeEventListener("input", adjustHeight);
   }, []);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  };
 
   const notice =
     "글 작성 시 유의사항\n\n인종, 성별, 출신, 지역, 이념 관련 차별 관련 발언 및 타인에게 불쾌감을 유발할 수 있는 글은 삭제될 수 있습니다.\n\n솔리드 커넥션은 홍보 행위를 철저히 금지합니다.";
   return (
     <div className={styles.form}>
-      <div className={styles.title}>
-        <textarea placeholder="제목을 입력하세요" maxLength={40} rows={2}></textarea>
+      <div className={styles.title} ref={titleRef}>
+        <textarea
+          placeholder="제목을 입력하세요"
+          maxLength={40}
+          rows={1}
+          ref={textareaRef}
+          onKeyDown={handleKeyDown}
+        ></textarea>
       </div>
       <div className={styles.question}>
         <div className={styles.question__select}>
