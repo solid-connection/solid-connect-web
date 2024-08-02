bdf68143dbf66d914a079e011cf42d4a5d443b04
devMuromi <devmuromi@gmail.com>
2024-08-02 13:02:18 +0900

    chore: svg 적용 설정 수정

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
