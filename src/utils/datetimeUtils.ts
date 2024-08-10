58531443f9447b5674af93be9e8dc998b986c05a
devMuromi <devmuromi@gmail.com>
2024-08-10 21:44:53 +0900

    feat: convertISODateToDateTime() 추가

--- src/utils/datetimeUtils.ts
+++ src/utils/datetimeUtils.ts
@@ -12,3 +12,20 @@ export const convertISODateToDate = (isoDate: string) => {
   // "YYYY. M. D." 형식으로 포맷
   return `${year}. ${month}. ${day}.`;
 };
+
+export const convertISODateToDateTime = (isoDate: string) => {
+  const date = new Date(isoDate);
+
+  if (isNaN(date.getTime())) {
+    return null;
+  }
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  // "YYYY. M. D. HH:MM" 형식으로 포맷
+  return `${year}. ${month}. ${day}. ${hour}:${minute}`;
+};
