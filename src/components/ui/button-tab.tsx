2a0f94f1de3b9b6b1e3ce15c89a7720ca1b12e4b
devMuromi <devmuromi@gmail.com>
2024-08-25 10:49:29 +0900

    design: 대학 목록 화면 버튼 색 변경

---
+++ src/components/ui/button-tab.tsx
@@ -0,0 +1,57 @@
+import styles from "./button-tab.module.css";
+
+type ButtonTabProps = {
+  choices: string[];
+  choice: string | null;
+  setChoice: (choice: string | null) => void;
+  style?: React.CSSProperties;
+  color?: {
+    activeBtn?: string;
+    deactiveBtn?: string;
+    activeBtnFont?: string;
+    deactiveBtnFont?: string;
+    background?: string;
+  };
+};
+
+export default function ButtonTab({ choices, choice, setChoice, style, color }: ButtonTabProps) {
+  // 디자인 색상
+  const defaultColor = {
+    activeBtn: "#6f90d1",
+    deactiveBtn: "#ffffff",
+    activeBtnFont: "#ffffff",
+    deactiveBtnFont: "black",
+    background: "#ffffff",
+  };
+  const resultColor = { ...defaultColor, ...color };
+
+  const handleButtonClick = (c) => {
+    // 이미 선택된 버튼을 다시 클릭할 경우 선택 취소
+    if (c === choice) {
+      setChoice(null);
+    } else {
+      setChoice(c);
+    }
+  };
+
+  return (
+    <div className={styles["tab-container"]} style={{ ...style, backgroundColor: resultColor.background }}>
+      {choices.map((c) => {
+        const isActive = c === choice;
+        return (
+          <div
+            key={c}
+            style={{
+              backgroundColor: isActive ? resultColor.activeBtn : resultColor.deactiveBtn,
+              color: isActive ? resultColor.activeBtnFont : resultColor.deactiveBtnFont,
+            }}
+            className={styles["tab-button"]}
+            onClick={() => handleButtonClick(c)}
+          >
+            <div>{c}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
