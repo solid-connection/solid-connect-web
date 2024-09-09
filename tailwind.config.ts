6f944003adda9f31c059f6e36af3bb1f0b3563eb
Wibaek Park <devmuromi@gmail.com>
2024-09-09 21:54:04 +0900

    chore: tailwind config 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
