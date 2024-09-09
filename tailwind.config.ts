9196afd985d96ad803a5893d26c09f7a8572a5a5
Wibaek Park <devmuromi@gmail.com>
2024-09-09 22:39:21 +0900

    chore: tailwind 폰트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    fontFamily: {
+      serif: ["Pretendard", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+    },
     extend: {},
   },
   plugins: [],
