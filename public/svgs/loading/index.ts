eb9013e8d97dc2a5d1d1ede7f489234d97946e39
Wibaek Park <devmuromi@gmail.com>
2024-10-29 21:39:45 +0900

    feat: CloudSpinner 추가

---
+++ public/svgs/loading/index.ts
@@ -0,0 +1,17 @@
+import IconCloudSpinner1 from "./cloud-spinner-1.svg";
+import IconCloudSpinner2 from "./cloud-spinner-2.svg";
+import IconCloudSpinner3 from "./cloud-spinner-3.svg";
+import IconCloudSpinner4 from "./cloud-spinner-4.svg";
+import IconCloudSpinner5 from "./cloud-spinner-5.svg";
+import IconCloudSpinner6 from "./cloud-spinner-6.svg";
+import IconCloudSpinner7 from "./cloud-spinner-7.svg";
+
+export {
+  IconCloudSpinner1,
+  IconCloudSpinner2,
+  IconCloudSpinner3,
+  IconCloudSpinner4,
+  IconCloudSpinner5,
+  IconCloudSpinner6,
+  IconCloudSpinner7,
+};
