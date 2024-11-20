71f4650ac6e256e7b8f8c7f27970924ee398400b
Wibaek Park <devmuromi@gmail.com>
2024-11-21 08:09:41 +0900

    fix: 파일명 대소문자 전환 git 반영이 안된 오류 수정

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+
+import BottomNavigation from "./BottomNavigation";
+
+import { useLayout } from "@/context/LayoutContext";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  const { hideBottomNavigation } = useLayout();
+  return (
+    <div className="mx-auto mb-[56px] w-full min-w-[360px] max-w-[600px] pt-[56px]">
+      {children}
+      {!hideBottomNavigation && <BottomNavigation />}
+    </div>
+  );
+};
+
+export default Layout;
