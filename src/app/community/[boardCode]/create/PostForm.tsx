5b74039bdc66cb02681307ede5d40ec6ba1f9cdf
Wibaek Park <devmuromi@gmail.com>
2024-12-10 21:56:45 +0900

    design: 프로필 이미지 없을때 사진 변경

--- src/app/community/[boardCode]/create/PostForm.tsx
+++ src/app/community/[boardCode]/create/PostForm.tsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react";
 
 import { createPostApi } from "@/services/community";
 
-import { IconArrowBackFilled, IconImage, IconPosstCheckboxOutlined, IconPostCheckboxFilled } from "@/public/svgs";
+import { IconArrowBackFilled, IconImage, IconPostCheckboxFilled, IconPostCheckboxOutlined } from "@/public/svgs";
 
 type PostFormProps = {
   boardCode: string;
@@ -103,7 +103,7 @@ const PostForm = ({ boardCode }: PostFormProps) => {
         <div className="flex h-[42px] items-center justify-between border-b border-b-gray-c-100 px-5 py-2.5">
           <div className="flex items-center gap-1 font-serif text-sm font-normal leading-[160%] text-[rgba(124,124,124,0.87)]">
             <button onClick={() => setIsQuestion(!isQuestion)} type="button">
-              {isQuestion ? <IconPostCheckboxFilled /> : <IconPosstCheckboxOutlined />}
+              {isQuestion ? <IconPostCheckboxFilled /> : <IconPostCheckboxOutlined />}
             </button>
             질문으로 업로드 하기
           </div>
@@ -152,7 +152,7 @@ const CustomTopDetailNavigation = ({ routeBack, submitPost }: CustomTopDetailNav
     <div className="font-serif text-base font-semibold leading-[160%] text-[rgba(0,0,0,0.87)]">글쓰기</div>
     <div className="min-w-6 cursor-pointer">
       <button
-        className="bg-secondary h-8 cursor-pointer rounded-full border-0 px-3 py-[5px] font-serif text-sm font-medium leading-[160%] text-white"
+        className="h-8 cursor-pointer rounded-full border-0 bg-secondary px-3 py-[5px] font-serif text-sm font-medium leading-[160%] text-white"
         onClick={submitPost}
         type="button"
       >
