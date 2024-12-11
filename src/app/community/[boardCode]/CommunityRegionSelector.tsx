7df8ef553c8e3a10dcd77881d8007ea0798aec7c
Wibaek Park <devmuromi@gmail.com>
2024-12-11 23:48:30 +0900

    feat: 드롭다운 외부 클릭시 드롭다운 닫기 기능 추가

--- src/app/community/[boardCode]/CommunityRegionSelector.tsx
+++ src/app/community/[boardCode]/CommunityRegionSelector.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import SlimDropdown from "@/components/ui/SlimDropdown";
 
@@ -13,12 +13,8 @@ type CommunityRegionSelectorProps = {
 };
 
 const CommunityRegionSelector = ({ curRegion, setCurRegion, regionChoices }: CommunityRegionSelectorProps) => {
-  // const [isRegionSelectorModalVisible, setIsRegionSelectorModalVisible] = useState<boolean>(false);
-  // const toggleRegionSelectorModal = () => {
-  //   setIsRegionSelectorModalVisible((prev) => !prev);
-  // };
-
   const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleDropdown = () => {
     setIsDropdownVisible((prev) => !prev);
@@ -34,9 +30,23 @@ const CommunityRegionSelector = ({ curRegion, setCurRegion, regionChoices }: Com
     action: () => handleSelectRegion(region.code),
   }));
 
+  // 외부 클릭 감지
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownVisible(false); // 외부 클릭 시 드롭다운 닫기
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="pb-3.5 pl-5 pt-5">
+    <div className="pb-3.5 pl-5 pt-5">
+      <div className="inline-block" ref={dropdownRef}>
         <button className="flex items-center gap-1" onClick={toggleDropdown} aria-label="게시판 변경">
           <div className="font-serif text-2xl font-semibold leading-normal">{curRegion}</div>
           <div>
@@ -45,7 +55,7 @@ const CommunityRegionSelector = ({ curRegion, setCurRegion, regionChoices }: Com
         </button>
         <div className="relative">{isDropdownVisible && <SlimDropdown options={dropdownOptions} />}</div>
       </div>
-    </>
+    </div>
   );
 };
 
