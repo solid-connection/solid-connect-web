14234779c1c5535fa422c3592a672a3ef914335c
Wibaek Park <devmuromi@gmail.com>
2025-01-20 04:10:32 +0900

    feat: 성적 관련 타입과 api 추가

--- src/services/score.ts
+++ src/services/score.ts
@@ -2,10 +2,20 @@ import { AxiosResponse } from "axios";
 
 import { axiosInstance } from "@/utils/axiosInstance";
 
-import { SubmitGpaScoreRequest, SubmitLanguageTestScoreRequest } from "@/types/score";
+import {
+  MyGpaScoreResponse,
+  MyLanguageTestScoreResponse,
+  SubmitGpaScoreRequest,
+  SubmitLanguageTestScoreRequest,
+} from "@/types/score";
 
 export const postGpaScoreApi = (request: SubmitGpaScoreRequest): Promise<AxiosResponse<null>> =>
-  axiosInstance.post("/score/gpaScore", request);
+  axiosInstance.post("/score/gpa", request);
 
 export const postLanguageTestScoreApi = (request: SubmitLanguageTestScoreRequest): Promise<AxiosResponse<null>> =>
-  axiosInstance.post("/score/languageTestScore", request);
+  axiosInstance.post("/score/languageTest", request);
+
+export const getMyGpaScoreApi = (): Promise<AxiosResponse<MyGpaScoreResponse>> => axiosInstance.get("/score/gpa");
+
+export const getMyLanguageTestScoreApi = (): Promise<AxiosResponse<MyLanguageTestScoreResponse>> =>
+  axiosInstance.get("/score/languageTest");
