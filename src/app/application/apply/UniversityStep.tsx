9ecb713d47ec0edaba3b046cc862d1e1d3762d60
Wibaek Park <devmuromi@gmail.com>
2025-01-25 15:35:42 +0900

    design: /application/apply 학교 선택 화면 디자인 추가

--- src/app/application/apply/UniversityStep.tsx
+++ src/app/application/apply/UniversityStep.tsx
@@ -16,40 +16,61 @@ const UniversityStep = ({ universityList, curUniversityList, setCurUniversityLis
     setCurUniversityList(newList);
   };
   return (
-    <div>
-      <div>해외 파견 학교를 검색하세요</div>
-      <div>
-        <label>1지망</label>
-        <select value={curUniversityList[0] ?? ""} onChange={(e) => handleSelect(0, Number(e.target.value))}>
-          <option value=""></option>
-          {universityList.map((university) => (
-            <option key={university.id} value={university.id}>
-              [{university.region} - {university.country}]{university.koreanName}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>2지망</label>
-        <select value={curUniversityList[1] ?? ""} onChange={(e) => handleSelect(1, Number(e.target.value))}>
-          <option value=""></option>
-          {universityList.map((university) => (
-            <option key={university.id} value={university.id}>
-              [{university.region} - {university.country}]{university.koreanName}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>3지망</label>
-        <select value={curUniversityList[2] ?? ""} onChange={(e) => handleSelect(2, Number(e.target.value))}>
-          <option value=""></option>
-          {universityList.map((university) => (
-            <option key={university.id} value={university.id}>
-              [{university.region} - {university.country}]{university.koreanName}
-            </option>
-          ))}
-        </select>
+    <>
+      <div className="px-5">
+        <div>
+          <span className="font-serif text-xs font-normal leading-normal text-[#acacac]">
+            본 과정 완료 후, 지원자 현황을 확인 할 수 있습니다.
+          </span>
+        </div>
+        <div className="mt-5">해외 파견 학교를 검색하세요.</div>
+        <div className="mt-5 flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label className="block font-serif text-sm font-semibold leading-normal text-k-900">1지망</label>
+            <select
+              className="flex h-10 items-center rounded-lg bg-k-50 px-5 py-2.5 font-serif text-sm font-semibold leading-normal text-primary"
+              value={curUniversityList[0] ?? ""}
+              onChange={(e) => handleSelect(0, Number(e.target.value))}
+            >
+              <option value=""></option>
+              {universityList.map((university) => (
+                <option key={university.id} value={university.id}>
+                  [{university.region} - {university.country}]{university.koreanName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="block font-serif text-sm font-semibold leading-normal text-k-900">1지망</label>
+            <select
+              className="flex h-10 items-center rounded-lg bg-k-50 px-5 py-2.5 font-serif text-sm font-semibold leading-normal text-primary"
+              value={curUniversityList[1] ?? ""}
+              onChange={(e) => handleSelect(1, Number(e.target.value))}
+            >
+              <option value=""></option>
+              {universityList.map((university) => (
+                <option key={university.id} value={university.id}>
+                  [{university.region} - {university.country}]{university.koreanName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="block font-serif text-sm font-semibold leading-normal text-k-900">1지망</label>
+            <select
+              className="flex h-10 items-center rounded-lg bg-k-50 px-5 py-2.5 font-serif text-sm font-semibold leading-normal text-primary"
+              value={curUniversityList[2] ?? ""}
+              onChange={(e) => handleSelect(2, Number(e.target.value))}
+            >
+              <option value=""></option>
+              {universityList.map((university) => (
+                <option key={university.id} value={university.id}>
+                  [{university.region} - {university.country}]{university.koreanName}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
       </div>
       <div className="fixed bottom-14 w-full max-w-[600px] bg-white">
         <div className="mb-[37px] px-5">
@@ -62,7 +83,7 @@ const UniversityStep = ({ universityList, curUniversityList, setCurUniversityLis
           </BlockBtn>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
