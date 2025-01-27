a0525f30c77dd99e40a59f75bd1ed9de886da037
Wibaek Park <devmuromi@gmail.com>
2025-01-28 02:03:25 +0900

    design: 지원서 제출 최종 완료 페이지 디자인 추가

--- src/app/application/apply/DoneStep.tsx
+++ src/app/application/apply/DoneStep.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import BlockBtn from "@/components/button/BlockBtn";
@@ -5,8 +6,9 @@ import BlockBtn from "@/components/button/BlockBtn";
 const DoneStep = () => {
   const router = useRouter();
   return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <div className="text-2xl font-bold">지원 완료</div>
+    <div className="mt-52 flex h-full flex-col items-center">
+      <Image src="/images/survey-complete-icon.png" width={120} height={120} alt="지원 완료" />
+      <div className="font-serif text-2xl font-semibold text-k-800">지원 완료</div>
 
       <div className="fixed bottom-14 w-full max-w-[600px] bg-white">
         <div className="mb-[37px] px-5">
