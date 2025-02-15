c72ee304b4993cf39db7aec344dad0398ad63993
Wibaek Park <devmuromi@gmail.com>
2025-02-15 16:42:05 +0900

    refactor: 대학, 내 정보 관련 api endpoint 변경, 대학 like 로직 변경

--- src/services/myInfo.ts
+++ src/services/myInfo.ts
@@ -5,19 +5,20 @@ import { axiosInstance } from "@/utils/axiosInstance";
 import { MyInfo, MyInfoSimple, MyNickname, MyProfileImage } from "@/types/myInfo";
 import { ListUniversity } from "@/types/university";
 
-export const getMyInfoApi = (): Promise<AxiosResponse<MyInfo>> => axiosInstance.get("/my-page");
+export const getMyInfoApi = (): Promise<AxiosResponse<MyInfo>> => axiosInstance.get("/my");
 
-export const getMyInfoSimpleApi = (): Promise<AxiosResponse<MyInfoSimple>> => axiosInstance.get("/my-page/update");
+export const getMyInfoSimpleApi = (): Promise<AxiosResponse<MyInfoSimple>> => axiosInstance.get("/my/update");
 
 export const updateMyInfoApi = (data: MyInfoSimple): Promise<AxiosResponse<MyInfoSimple>> =>
-  // DEPRECATED: Use updateMyNicknameApi and updateMyProfileImage instead
-  axiosInstance.patch("/my-page/update", data);
+  axiosInstance.patch("/my/update", data);
 
 export const getMyWishUniversityApi = (): Promise<AxiosResponse<ListUniversity[]>> =>
   axiosInstance.get("/university/like");
 
 export const updateMyNicknameApi = (nickname: string): Promise<AxiosResponse<MyNickname>> =>
-  axiosInstance.patch("/my-page/update/nickname", { nickname });
+  // DEPRECATED: Use updateMyInfoApi instead
+  axiosInstance.patch("/my/update/nickname", { nickname });
 
 export const updateMyProfileImage = (file: File): Promise<AxiosResponse<MyProfileImage>> =>
-  axiosInstance.patch("/my-page/update/profileImage", { file }, { headers: { "Content-Type": "multipart/form-data" } });
+  // DEPRECATED: Use updateMyInfoApi instead
+  axiosInstance.patch("/my/update/profileImage", { file }, { headers: { "Content-Type": "multipart/form-data" } });
