88a1d6198a54986aa15767f94acc4fe24b869f8c
Wibaek Park <devmuromi@gmail.com>
2025-02-16 22:38:58 +0900

    design: TopNavigation 디자인 변경

--- public/svgs/home/index.ts
+++ public/svgs/home/index.ts
@@ -1,6 +1,7 @@
+import IconCloud from "./cloud.svg";
 import IconIdCard from "./id-card.svg";
 import IconMagnifyingGlass from "./magnifying-glass.svg";
 import IconMuseum from "./museum.svg";
 import IconPaper from "./paper.svg";
 
-export { IconIdCard, IconMagnifyingGlass, IconMuseum, IconPaper };
+export { IconCloud, IconIdCard, IconMagnifyingGlass, IconMuseum, IconPaper };
