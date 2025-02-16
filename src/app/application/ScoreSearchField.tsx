334fe253651c21947a944feeb63aae16d37e638a
Wibaek Park <devmuromi@gmail.com>
2025-02-16 17:13:24 +0900

    feat: 성적비교 페이지 복구

---
+++ src/app/application/ScoreSearchField.tsx
@@ -0,0 +1,21 @@
+import styles from "./score-search-field.module.css";
+
+type ScoreSearchFieldProps = {
+  keyWords: string[];
+  setKeyWord: (keyWord: string) => void;
+};
+
+const ScoreSearchField = ({ keyWords, setKeyWord }: ScoreSearchFieldProps) => (
+  <div>
+    <div className={styles.title}>인기 검색</div>
+    <div className={styles.container}>
+      {keyWords.map((keyWord) => (
+        <button key={keyWord} className={styles.item} onClick={() => setKeyWord(keyWord)} type="button">
+          {keyWord}
+        </button>
+      ))}
+    </div>
+  </div>
+);
+
+export default ScoreSearchField;
