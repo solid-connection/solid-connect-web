a45e0ff0b2b3b9fafe31d2120f8233ac09c9c27d
Wibaek Park <devmuromi@gmail.com>
2025-02-16 22:22:08 +0900

    feat: 새로운 홈 화면 일부 적용

---
+++ public/svgs/home/index.ts
@@ -0,0 +1,6 @@
+import IconIdCard from "./id-card.svg";
+import IconMagnifyingGlass from "./magnifying-glass.svg";
+import IconMuseum from "./museum.svg";
+import IconPaper from "./paper.svg";
+
+export { IconIdCard, IconMagnifyingGlass, IconMuseum, IconPaper };
