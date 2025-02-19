1c62303babc33b2f09a842e0dfb762316ae186c6
Wibaek Park <34394229+wibaek@users.noreply.github.com>
2025-02-20 02:27:57 +0900

    fix: 어학 성적 타입 Enum 추가, 어학 종류 추가 (#117)

--- src/utils/scoreUtils.ts
+++ src/utils/scoreUtils.ts
@@ -1,29 +1,31 @@
+import { LanguageTestEnum } from "@/types/score";
+
 export const validateLanguageScore = (testType: string, score: string) => {
   const numScore = Number(score);
 
-  if (testType === "TOEIC") {
+  if (testType === LanguageTestEnum.TOEIC) {
     if (!(numScore >= 0 && numScore <= 990)) {
       alert("TOEIC 점수는 0 ~ 990 사이여야 합니다.");
       throw new Error("TOEIC 점수는 0 ~ 990 사이여야 합니다.");
     }
   }
-  if (testType === "TOEFL IBT") {
+  if (testType === LanguageTestEnum.TOEFL_IBT) {
     if (!(numScore >= 0 && numScore <= 120)) {
       alert("TOEFL IBT 점수는 0 ~ 120 사이여야 합니다.");
       throw new Error("TOEFL IBT 점수는 0 ~ 120 사이여야 합니다.");
     }
   }
-  if (testType === "TOEFL ITP") {
+  if (testType === LanguageTestEnum.TOEFL_ITP) {
     if (!(numScore >= 310 && numScore <= 677)) {
       throw new Error("TOEFL ITP 점수는 310 ~ 677 사이여야 합니다.");
     }
   }
-  if (testType === "IELTS") {
+  if (testType === LanguageTestEnum.IELTS) {
     if (!(numScore >= 0 && numScore <= 9)) {
       throw new Error("IELTS 점수는 0 ~ 9 사이여야 합니다.");
     }
   }
-  if (testType === "JLPT") {
+  if (testType === LanguageTestEnum.JLPT) {
     if (!(numScore >= 1 && numScore <= 5)) {
       throw new Error("JLPT 등급은 1 ~ 5 사이여야 합니다.");
     }
