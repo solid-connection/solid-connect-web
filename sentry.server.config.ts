6db52e5023a3b9fdeb58d623da64f2ced12b244e
Wibaek Park <devmuromi@gmail.com>
2025-02-20 11:56:13 +0900

    chore: sentry 개발 환경 local -> development로 변경

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || "",
-  environment: process.env.SENTRY_ENVIRONMENT || "local",
+  environment: process.env.SENTRY_ENVIRONMENT || "development",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
