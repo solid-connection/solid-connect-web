8538b5be1543093221df6cd8299faeae224ba5de
Wibaek Park <devmuromi@gmail.com>
2025-02-24 11:10:30 +0900

    refactor: BlockBtn 리팩토링, disabled시 디자인 처리

--- src/components/button/BlockBtn.tsx
+++ src/components/button/BlockBtn.tsx
@@ -1,16 +1,21 @@
-type BlockBtnProps = {
+import React from "react";
+
+export interface BlockBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
   children: React.ReactNode;
-};
+}
 
-const BlockBtn = ({ onClick, children }: BlockBtnProps) => (
+const BlockBtn = React.forwardRef<HTMLButtonElement, BlockBtnProps>(({ onClick, children, ...props }, ref) => (
   <button
-    className="h-[3.125rem] w-full min-w-80 max-w-screen-sm rounded-lg bg-primary"
+    ref={ref}
+    className="h-[3.125rem] w-full min-w-80 max-w-screen-sm rounded-lg bg-primary disabled:opacity-50"
     onClick={onClick}
     type="button"
+    {...props}
   >
     <span className="font-serif text-base font-medium text-white">{children}</span>
   </button>
-);
+));
+BlockBtn.displayName = "BlockBtn";
 
 export default BlockBtn;
