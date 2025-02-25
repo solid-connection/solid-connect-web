9354b7bf0f148456820d43df503178bf9afa9f14
Wibaek Park <devmuromi@gmail.com>
2025-02-25 22:34:37 +0900

    design: BlockBtn 색상 변경

--- src/components/button/BlockBtn.tsx
+++ src/components/button/BlockBtn.tsx
@@ -4,23 +4,21 @@ import { type VariantProps, cva } from "class-variance-authority";
 
 import { cn } from "@/utils/designUtils";
 
-const blockBtnVariants = cva(
-  "h-13 w-full min-w-80 max-w-screen-sm rounded-lg disabled:opacity-50 flex items-center justify-center",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary hover:bg-primary/90",
-      },
-      text: {
-        default: "text-base font-medium text-white",
-      },
+const blockBtnVariants = cva("h-13 w-full min-w-80 max-w-screen-sm rounded-lg flex items-center justify-center", {
+  variants: {
+    variant: {
+      default: "bg-secondary hover:bg-secondary/90 disabled:bg-k-100",
+      primary: "bg-primary hover:bg-primary/90 disabled:bg-k-100",
     },
-    defaultVariants: {
-      variant: "default",
-      text: "default",
+    text: {
+      default: "text-base font-medium text-white",
     },
   },
-);
+  defaultVariants: {
+    variant: "default",
+    text: "default",
+  },
+});
 
 export interface BlockBtnProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -30,15 +28,9 @@ export interface BlockBtnProps
 }
 
 const BlockBtn = React.forwardRef<HTMLButtonElement, BlockBtnProps>(
-  ({ className, variant, onClick, children, ...props }, ref) => {
+  ({ className, variant, text, children, ...props }, ref) => {
     return (
-      <button
-        ref={ref}
-        onClick={onClick}
-        type="button"
-        className={cn(blockBtnVariants({ variant, className }))}
-        {...props}
-      >
+      <button ref={ref} className={cn(blockBtnVariants({ variant, text, className }))} {...props}>
         <span>{children}</span>
       </button>
     );
