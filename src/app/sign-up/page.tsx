4d2c45f39dd1c4c63c510feb422100479366ca22
Wibaek Park <devmuromi@gmail.com>
2025-03-27 00:23:54 +0900

    feat: 회원가입 페이지 레이아웃 다시 추가

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,26 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import SignupSurvey from "@/components/login/signup/SignupSurvey";
 
-import { useLayout } from "@/context/LayoutContext";
-
 const SignUpPage = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const signUpToken = searchParams?.get("token");
 
-  const { setHideBottomNavigation } = useLayout();
-
-  useEffect(() => {
-    setHideBottomNavigation(true);
-    return () => setHideBottomNavigation(false); // 컴포넌트 언마운트 시 다시 보이게 설정
-  }, [setHideBottomNavigation]);
-
   if (!signUpToken) {
-    // 토큰이 없을 경우 처리 (예: 로그인 페이지로 리다이렉트)
-    return <div>유효하지 않은 접근입니다.</div>;
+    router.push("/login");
   }
 
   return (
