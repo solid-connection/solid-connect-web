d5c1038638ac0c8e09ac748d5318bd4f6db2080e
Wibaek Park <devmuromi@gmail.com>
2025-03-29 13:04:40 +0900

    feat: 내 정보 관련 api 변경

--- src/services/myInfo.ts
+++ src/services/myInfo.ts
@@ -2,23 +2,24 @@ import { AxiosResponse } from "axios";
 
 import { axiosInstance } from "@/utils/axiosInstance";
 
-import { MyInfo, MyInfoSimple, MyNickname, MyProfileImage } from "@/types/myInfo";
+import { MyInfo, MyInfoPatchRequest, MyInfoSimple } from "@/types/myInfo";
 import { ListUniversity } from "@/types/university";
 
 export const getMyInfoApi = (): Promise<AxiosResponse<MyInfo>> => axiosInstance.get("/my");
 
-export const getMyInfoSimpleApi = (): Promise<AxiosResponse<MyInfoSimple>> => axiosInstance.get("/my/update");
+export const updateMyInfoApi = (request: MyInfoPatchRequest): Promise<AxiosResponse<MyInfoSimple>> => {
+  const convertedRequest: FormData = new FormData();
+  if (request.nickname) {
+    convertedRequest.append("nickname", request.nickname);
+  }
+  if (request.file) {
+    convertedRequest.append("file", request.file);
+  }
 
-export const updateMyInfoApi = (data: MyInfoSimple): Promise<AxiosResponse<MyInfoSimple>> =>
-  axiosInstance.patch("/my/update", data);
+  return axiosInstance.patch("/my", convertedRequest, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
 
 export const getMyWishUniversityApi = (): Promise<AxiosResponse<ListUniversity[]>> =>
   axiosInstance.get("/universities/like");
-
-export const updateMyNicknameApi = (nickname: string): Promise<AxiosResponse<MyNickname>> =>
-  // DEPRECATED: Use updateMyInfoApi instead
-  axiosInstance.patch("/my/update/nickname", { nickname });
-
-export const updateMyProfileImage = (file: File): Promise<AxiosResponse<MyProfileImage>> =>
-  // DEPRECATED: Use updateMyInfoApi instead
-  axiosInstance.patch("/my/update/profileImage", { file }, { headers: { "Content-Type": "multipart/form-data" } });
