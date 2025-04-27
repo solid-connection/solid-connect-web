e1857be3ad06b140b85dddac7b4a999955b490e1
Wibaek Park <devmuromi@gmail.com>
2025-04-27 21:42:35 +0900

    design: UniversityCards 디자인 변경

--- src/components/college/UniversityCards.tsx
+++ src/components/college/UniversityCards.tsx
@@ -13,7 +13,7 @@ type UniversityCardsProps = {
 };
 
 const UniversityCards = ({ colleges, style }: UniversityCardsProps) => (
-  <div className="flex flex-col gap-2" style={style}>
+  <div className="flex flex-col gap-2.5" style={style}>
     {colleges.map((university) => (
       <UniversityCard key={university.id} university={university} />
     ))}
@@ -30,14 +30,15 @@ export const UniversityCard = ({ university }: UniversityCardProps) => {
     university.term !== process.env.NEXT_PUBLIC_CURRENT_TERM
       ? `${university.koreanName}(${university.term})`
       : university.koreanName;
+
   return (
     <Link
-      className="relative mx-5 flex h-[99px] overflow-hidden rounded-md border border-solid border-[#eaeaea] no-underline hover:-translate-y-0.5 hover:shadow-md hover:shadow-black/10"
+      className="relative mx-5 flex h-[91px] overflow-hidden rounded-lg border border-solid border-k-100 px-5 py-3 hover:-translate-y-0.5 hover:shadow-md hover:shadow-black/10"
       href={`/university/${university.id}`}
     >
-      <div className="flex items-center">
+      <div className="flex flex-shrink-0 items-center">
         <Image
-          className="ml-3 h-[60px] w-[60px] rounded-[60px] object-cover"
+          className="h-14 w-14 rounded-full object-cover"
           src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${university.logoImageUrl}`}
           width={100}
           height={100}
@@ -45,33 +46,32 @@ export const UniversityCard = ({ university }: UniversityCardProps) => {
         />
       </div>
 
-      <div className="ml-4 flex flex-col no-underline">
-        <span className="mt-[18px] font-serif text-base font-semibold leading-normal text-black">
-          {convertedKoreanName}
-        </span>
-        <div className="flex gap-[3px]">
-          <span className="mt-1.5 font-serif text-sm font-semibold leading-[150%] text-[#7c7c7c]">
+      <div className="ml-[22px] flex flex-grow flex-col">
+        <span className="truncate text-base font-bold leading-normal text-k-700">{convertedKoreanName}</span>
+        <div className="flex items-center justify-between">
+          <span className="text-sm font-medium leading-normal text-k-500">
             {university.country} | {university.region}
           </span>
-          <span className="ml-2.5 mt-1.5 font-serif text-xs font-semibold leading-[150%] text-[#7c7c7c]">
-            모집 {university.studentCapacity}명
-          </span>
+          <div className="flex items-center">
+            <span className="text-xs font-semibold leading-normal text-primary">
+              모집 {university.studentCapacity}명
+            </span>
+            <CheveronRightFilled color="black" opacity="0.54" />
+          </div>
         </div>
         <div className="flex gap-4">
           {university.languageRequirements.slice(0, 3).map((requirement) => (
             <span
               key={requirement.languageTestType}
-              className="whitespace-nowrap font-serif text-xs font-semibold leading-[150%] tracking-[0.15px] text-[#7c7c7c]"
+              className="whitespace-nowrap text-xs font-medium leading-normal text-k-500"
             >
-              {shortenLanguageTestName(requirement.languageTestType)}: {requirement.minScore}
+              {shortenLanguageTestName(requirement.languageTestType)} {requirement.minScore}
             </span>
           ))}
         </div>
       </div>
 
-      <div className="absolute right-[9.77px] top-[38px] h-6 w-6">
-        <CheveronRightFilled color="black" opacity="0.54" />
-      </div>
+      <div className="absolute right-[9.77px] top-[38px] h-6 w-6"></div>
     </Link>
   );
 };
