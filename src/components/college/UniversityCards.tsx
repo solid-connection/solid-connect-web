681a72fbcb5158b55f0ead613ffc0b6617e325f1
Wibaek Park <34394229+wibaek@users.noreply.github.com>
2025-05-30 15:19:42 +0900

    design: TopNavigation, UniversityCard 디자인 변경, 리팩토링 (#183)
    * design: TopNavigation 배경 색 변경
    
    * design: UniversityCard 디자인 변경, 리팩토링
    
    * refactor: Home에서 UniversiyCards 사용하게 변경
    
    * remove: 미사용 import 삭제

--- src/components/college/UniversityCards.tsx
+++ src/components/college/UniversityCards.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import clsx from "clsx";
+
 import { shortenLanguageTestName } from "@/utils/universityUtils";
 
 import CheveronRightFilled from "@/components/ui/icon/ChevronRightFilled";
@@ -10,12 +12,14 @@ import { ListUniversity } from "@/types/university";
 type UniversityCardsProps = {
   colleges: ListUniversity[];
   style?: React.CSSProperties;
+  className?: string;
+  showCapacity?: boolean;
 };
 
-const UniversityCards = ({ colleges, style }: UniversityCardsProps) => (
-  <div className="flex flex-col gap-2.5" style={style}>
+const UniversityCards = ({ colleges, style, className, showCapacity = true }: UniversityCardsProps) => (
+  <div className={clsx("flex flex-col gap-2.5", className)} style={style}>
     {colleges.map((university) => (
-      <UniversityCard key={university.id} university={university} />
+      <UniversityCard key={university.id} university={university} showCapacity={showCapacity} />
     ))}
   </div>
 );
@@ -23,9 +27,10 @@ export default UniversityCards;
 
 type UniversityCardProps = {
   university: ListUniversity;
+  showCapacity?: boolean;
 };
 
-export const UniversityCard = ({ university }: UniversityCardProps) => {
+export const UniversityCard = ({ university, showCapacity = true }: UniversityCardProps) => {
   const convertedKoreanName =
     university.term !== process.env.NEXT_PUBLIC_CURRENT_TERM
       ? `${university.koreanName}(${university.term})`
@@ -33,43 +38,61 @@ export const UniversityCard = ({ university }: UniversityCardProps) => {
 
   return (
     <Link
-      className="relative mx-5 flex h-[91px] overflow-hidden rounded-lg border border-solid border-k-100 px-5 py-3 hover:-translate-y-0.5 hover:shadow-md hover:shadow-black/10"
+      className="relative h-[91px] overflow-hidden rounded-lg border border-solid border-k-100 hover:-translate-y-0.5 hover:shadow-md hover:shadow-black/10"
       href={`/university/${university.id}`}
     >
-      <div className="flex flex-shrink-0 items-center">
-        <Image
-          className="h-14 w-14 rounded-full object-cover"
-          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${university.logoImageUrl}`}
-          width={100}
-          height={100}
-          alt={convertedKoreanName || "대학 이미지"}
-        />
-      </div>
+      <div className="flex justify-between px-5 py-3.5">
+        <div className="flex gap-[23.5px]">
+          <UniversityLogo logoImageUrl={university.logoImageUrl} />
 
-      <div className="ml-[22px] flex flex-grow flex-col">
-        <span className="truncate text-base font-bold leading-normal text-k-700">{convertedKoreanName}</span>
-        <div className="flex items-center justify-between">
-          <span className="text-sm font-medium leading-normal text-k-500">
-            {university.country} | {university.region}
-          </span>
-          <div className="flex items-center">
-            <span className="text-xs font-semibold leading-normal text-primary">
-              모집 {university.studentCapacity}명
-            </span>
-            <CheveronRightFilled color="black" opacity="0.54" />
+          <div className="flex flex-col">
+            <span className="truncate text-base font-bold leading-normal text-k-700">{convertedKoreanName}</span>
+            <div className="flex items-center gap-2.5">
+              <span className="text-xs font-medium leading-normal text-k-500">
+                {university.country} | {university.region}
+              </span>
+              {showCapacity && (
+                <span className="text-xs font-semibold leading-normal text-primary">
+                  모집 {university.studentCapacity}명
+                </span>
+              )}
+            </div>
+            <LanguageRequirements languageRequirements={university.languageRequirements} />
           </div>
         </div>
-        <div className="flex gap-4">
-          {university.languageRequirements.slice(0, 3).map((requirement) => (
-            <span
-              key={requirement.languageTestType}
-              className="whitespace-nowrap text-xs font-medium leading-normal text-k-500"
-            >
-              {shortenLanguageTestName(requirement.languageTestType)} {requirement.minScore}
-            </span>
-          ))}
+        <div className="flex items-center">
+          <CheveronRightFilled color="black" opacity="0.54" />
         </div>
       </div>
     </Link>
   );
 };
+
+const UniversityLogo = ({ logoImageUrl }: { logoImageUrl: string }) => (
+  <div className="flex flex-shrink-0 items-center">
+    <Image
+      className="h-14 w-14 rounded-full object-cover"
+      src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${logoImageUrl}`}
+      width={62}
+      height={62}
+      alt="대학 이미지"
+    />
+  </div>
+);
+
+const LanguageRequirements = ({
+  languageRequirements,
+}: {
+  languageRequirements: ListUniversity["languageRequirements"];
+}) => (
+  <div className="flex gap-4">
+    {languageRequirements.slice(0, 3).map((requirement) => (
+      <span
+        key={requirement.languageTestType}
+        className="whitespace-nowrap text-xs font-medium leading-normal text-k-500"
+      >
+        {shortenLanguageTestName(requirement.languageTestType)} {requirement.minScore}
+      </span>
+    ))}
+  </div>
+);
--- src/components/layout/TopNavigation.tsx
+++ src/components/layout/TopNavigation.tsx
@@ -1,7 +1,10 @@
 import { IconCloud } from "@/public/svgs/home";
 
 const TopNavigation = () => (
-  <div className="fixed top-0 z-30 flex h-[56px] w-full max-w-[600px] items-center bg-primary">
+  <div
+    className="fixed top-0 z-30 flex h-14 w-full max-w-[600px] items-center"
+    style={{ background: "linear-gradient(269deg, var(--Primary-Color, #5950F6) 1.26%, #2AA4E2 100%)" }}
+  >
     <div className="flex items-center gap-1.5 pl-5">
       <IconCloud />
       <span className="text-[17px] font-semibold text-k-0">Solid Connection</span>
