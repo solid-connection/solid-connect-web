b6e77e7ac18fc3357e3125d5083a1da20d774246
Wibaek Park <devmuromi@gmail.com>
2025-06-21 14:23:31 +0900

    fix: 이미지 오버플로우 문제 수정

--- src/app/university/[id]/CollegeDetail.tsx
+++ src/app/university/[id]/CollegeDetail.tsx
@@ -8,7 +8,7 @@ const CollegeDetail = ({ imageUrl }: CollegeDetailProps) => (
   <div className="fixed top-14 -z-20 h-[100vw] w-full">
     <div className="mac-w-[600px] absolute -z-10 h-[67vw] max-h-[400px] w-full">
       <Image
-        className="h-full w-full object-cover"
+        className="h-full object-cover"
         src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${imageUrl}`}
         width={600}
         height={300}
