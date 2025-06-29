6ab537809244142796a5264773dd72d3481d93e1
Wibaek Park <devmuromi@gmail.com>
2025-06-29 18:53:52 +0900

    refactor: UniversityDetail 과도한 컴포넌트 분리 해제

--- src/app/university/[id]/UniversityDetail.tsx
+++ src/app/university/[id]/UniversityDetail.tsx
@@ -17,50 +17,43 @@ interface UniversityDetailProps {
 const UniversityDetail = ({ university }: UniversityDetailProps) => {
   return (
     <>
-      <UniversityImage imageUrl={university.backgroundImageUrl} />
-      <UniversityInfo university={university} />
+      <div className="h-[236px] bg-blue-100 object-cover">
+        <Image
+          alt="대학 이미지"
+          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${university.backgroundImageUrl}`}
+          width={390}
+          height={236}
+        />
+      </div>
+      <div className="-mt-[66px] rounded-t-3xl bg-white px-5">
+        <TitleSection
+          title={university.formatName}
+          subTitle={university.englishName}
+          logoUrl={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${university.logoImageUrl}`}
+        />
+        {/* TODO: totalDispatchCount 추가시 연동, 나라에 국기 추가 */}
+        <SubTitleSection
+          totalDispatchCount={0}
+          country={university.country}
+          studentCapacity={university.studentCapacity}
+        />
+        <LanguageSection
+          detailsForLanguage={university.detailsForLanguage}
+          languageRequirements={university.languageRequirements}
+        />
+        <InfoSection
+          semesterRequirement={university.semesterRequirement}
+          semesterAvailableForDispatch={university.semesterAvailableForDispatch}
+          detailsForApply={university.detailsForApply}
+          detailsForAccommodation={university.detailsForAccommodation}
+        />
+        <MajorSection majorDetail={university.detailsForMajor} />
+        <EnglishSection englishDetail={university.detailsForEnglishCourse} />
+        <MapSection universityEnglishName={university.englishName} />
+        <div className="h-48" />
+      </div>
     </>
   );
 };
 
 export default UniversityDetail;
-
-const UniversityImage = ({ imageUrl }: { imageUrl: string }) => {
-  return (
-    <div className="h-[236px] bg-blue-100 object-cover">
-      <Image alt="대학 이미지" src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${imageUrl}`} width={390} height={236} />
-    </div>
-  );
-};
-
-const UniversityInfo = ({ university }: { university: University }) => {
-  return (
-    <div className="-mt-[66px] rounded-t-3xl bg-white px-5">
-      <TitleSection
-        title={university.formatName}
-        subTitle={university.englishName}
-        logoUrl={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${university.logoImageUrl}`}
-      />
-      {/* TODO: totalDispatchCount 추가시 연동, 나라에 국기 추가 */}
-      <SubTitleSection
-        totalDispatchCount={0}
-        country={university.country}
-        studentCapacity={university.studentCapacity}
-      />
-      <LanguageSection
-        detailsForLanguage={university.detailsForLanguage}
-        languageRequirements={university.languageRequirements}
-      />
-      <InfoSection
-        semesterRequirement={university.semesterRequirement}
-        semesterAvailableForDispatch={university.semesterAvailableForDispatch}
-        detailsForApply={university.detailsForApply}
-        detailsForAccommodation={university.detailsForAccommodation}
-      />
-      <MajorSection majorDetail={university.detailsForMajor} />
-      <EnglishSection englishDetail={university.detailsForEnglishCourse} />
-      <MapSection universityEnglishName={university.englishName} />
-      <div className="h-48" />
-    </div>
-  );
-};
