dbd83c443b2c66696546a21fdf3274286028254a
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-08-03 13:30:41 +0900

    refactor : newsSection 분리및 코드 스플리팅 적용했습니다

---
+++ src/app/_home/_ui/NewsSection/index.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+import { News } from "@/types/news";
+
+import { IconSpeaker } from "@/public/svgs";
+
+export type NewsSectionProps = {
+  newsList: News[];
+};
+
+const NewsSection = async ({ newsList }: NewsSectionProps) => (
+  <div className="mt-6 pl-5">
+    <div className="mb-2.5 flex items-center gap-1.5 font-serif text-base font-semibold text-k-700">
+      솔커에서 맛보는 소식
+      <IconSpeaker />
+    </div>
+    <div className="flex flex-col gap-4">
+      {newsList.map((news) => (
+        <a key={news.id} target="_blank" href={news.url} rel="noreferrer">
+          <div className="flex gap-4">
+            <Image
+              loading="lazy"
+              className="h-24 w-44 shrink-0 rounded-xl object-cover"
+              src={news.imageUrl}
+              alt={news.title}
+              width={170}
+              height={90}
+            />
+            <div className="mr-5 flex flex-col gap-0.5">
+              <div className="text-serif text-sm font-semibold leading-normal text-k-700">{news.title}</div>
+              <div className="font-serif text-xs font-normal leading-normal text-k-500">{news.description}</div>
+            </div>
+          </div>
+        </a>
+      ))}
+    </div>
+  </div>
+);
+
+export default NewsSection;
