df2072afa56db945e3edd9a72b9990ca54ad9320
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-08-26 02:20:05 +0900

    refactor : spinner 위치 조정했습니다

--- src/components/layout/GlobalLayout/ui/ClientModal/index.tsx
+++ src/components/layout/GlobalLayout/ui/ClientModal/index.tsx
@@ -17,8 +17,8 @@ const ClientModal = () => {
 
   return (
     <>
-      {isFetching ? (
-        <div aria-live="polite" aria-busy="true" className="fixed bottom-20 left-1/2 z-50 -translate-x-1/2">
+      {!isFetching ? (
+        <div aria-live="polite" aria-busy="true" className="fixed bottom-40 left-1/2 z-50 -translate-x-1/2">
           <CloudSpinner />
         </div>
       ) : null}
