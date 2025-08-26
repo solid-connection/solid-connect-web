f95b03db867c2e599a4d62a064f56d61b66285fc
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-08-26 18:29:47 +0900

    fix : 잘못된 페이지로 이동되는 로직 수정했습니다

--- src/api/applications/client/usePostSubmitApplication.ts
+++ src/api/applications/client/usePostSubmitApplication.ts
@@ -28,8 +28,6 @@ export const postSubmitApplication = (
 // 타입 경로
 
 const usePostSubmitApplication = () => {
-  const router = useRouter();
-
   return useMutation({
     // mutationFn: API 요청을 수행할 비동기 함수를 지정합니다.
     mutationFn: (request: UseSubmitApplicationRequest) => postSubmitApplication(request),
@@ -37,14 +35,6 @@ const usePostSubmitApplication = () => {
     // onSuccess: API 요청이 성공했을 때 실행할 콜백 함수입니다.
     onSuccess: (data) => {
       console.log("지원이 성공적으로 완료되었습니다.", data);
-      alert("지원이 완료되었습니다.");
-
-      // 성공 후, 관련된 다른 데이터들을 최신 상태로 업데이트하고 싶을 때 사용합니다.
-      // 예를 들어, '내 지원 목록' 데이터를 다시 불러옵니다.
-      // queryClient.invalidateQueries({ queryKey: ['myApplications'] });
-
-      // 지원 완료 페이지로 이동합니다.
-      router.push("/application/complete");
     },
 
     // onError: API 요청이 실패했을 때 실행할 콜백 함수입니다.
