8b2bfe087bdb146cbc1d46f6a7dd443c4f522353
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-08-27 16:43:11 +0900

    fix : 지원완료 페이지 수정했습니다

--- src/app/university/application/apply/DoneStep.tsx
+++ src/app/university/application/apply/DoneStep.tsx
@@ -6,20 +6,31 @@ import BlockBtn from "@/components/button/BlockBtn";
 const DoneStep = () => {
   const router = useRouter();
   return (
-    <div className="mt-52 flex h-full flex-col items-center">
+    <div className="mt-52 flex h-full flex-col items-center justify-center gap-6">
       <Image src="/images/survey-complete-icon.png" width={120} height={120} alt="지원 완료" />
-      <div className="font-serif text-2xl font-semibold text-k-800">지원 완료</div>
+      <div className="text-center font-serif text-2xl font-semibold text-k-800">
+        학교 지원이
+        <br />
+        <span className="text-secondary">완료</span>
+        되었어요!
+      </div>
 
-      <div className="fixed bottom-14 w-full max-w-[600px] bg-white">
-        <div className="mb-[37px] px-5">
-          <BlockBtn
-            onClick={() => {
-              router.push("/");
-            }}
-          >
-            홈으로
-          </BlockBtn>
-        </div>
+      <div className="mt-10 flex w-full flex-col gap-3 px-5 pt-8">
+        <BlockBtn
+          className="bg-primary-900 text-white"
+          onClick={() => {
+            router.push("/university/application");
+          }}
+        >
+          지원자 현황 보기
+        </BlockBtn>
+        <BlockBtn
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          홈으로
+        </BlockBtn>
       </div>
     </div>
   );
