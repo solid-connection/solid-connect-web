44080a6d70d2f194b6f7a44cefec851eaf6141e9
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-09-06 17:30:27 +0900

    refactor : 커뮤니티 리액트 쿼리로 수정

---
+++ src/api/boards/clients/useGetPostList.ts
@@ -0,0 +1,34 @@
+import { AxiosResponse } from "axios";
+
+import { axiosInstance } from "@/utils/axiosInstance";
+
+import { QueryKeys } from "../../my/client/queryKey";
+
+import { ListPost } from "@/types/community";
+
+import { useQuery } from "@tanstack/react-query";
+
+interface UseGetPostListProps {
+  boardCode: string;
+  category?: string | null;
+}
+
+const getPostList = (boardCode: string, category: string | null = null): Promise<AxiosResponse<ListPost[]>> =>
+  axiosInstance.get(`/boards/${boardCode}`, {
+    params: {
+      category,
+    },
+  });
+
+const useGetPostList = ({ boardCode, category = "전체" }: UseGetPostListProps) => {
+  return useQuery({
+    queryKey: [QueryKeys.myInfo],
+    queryFn: () => getPostList(boardCode, category),
+    // staleTime을 무한으로 설정하여 불필요한 자동 refetch를 방지합니다.
+    staleTime: Infinity,
+    gcTime: 1000 * 60 * 30, // 예: 30분
+    select: (response) => response.data, // response.data가 실제 ListPost[]
+  });
+};
+
+export default useGetPostList;
