320a5f9a5103881a8327594e30702145c7c5c594
manNomi <hanmw110@naver.com>
2025-10-09 23:40:34 +0900

    fix : route 버그 수정

--- src/components/mentor/MentorCard/index.tsx
+++ src/components/mentor/MentorCard/index.tsx
@@ -107,7 +107,7 @@ const MentorCard = ({ mentor, observeRef, isMine = false }: MentorCardProps) => 
             ) : (
               <>
                 <Link
-                  href={`mentor/${id}`}
+                  href={`/mentor/${id}`}
                   className="flex h-10 w-1/2 flex-shrink-0 items-center justify-center gap-3 rounded-[20px] bg-primary px-5 py-2.5 font-medium text-white"
                 >
                   멘토 페이지
