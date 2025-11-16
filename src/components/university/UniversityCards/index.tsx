99e23eba6b7bfed6fd321fe7a6fb33d3e3671e98
manNomi <hanmw110@naver.com>
2025-11-16 05:36:30 +0000

    UniversityCards에 react-virtual 적용

--- src/components/university/UniversityCards/index.tsx
+++ src/components/university/UniversityCards/index.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useRef } from "react";
+
 import clsx from "clsx";
+
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 import UniversityCard from "../../ui/UniverSityCard";
 
@@ -11,11 +17,51 @@ type UniversityCardsProps = {
   showCapacity?: boolean;
 };
 
-const UniversityCards = ({ colleges, style, className, showCapacity = true }: UniversityCardsProps) => (
-  <div className={clsx("flex flex-col gap-2.5", className)} style={style}>
-    {colleges.map((university) => (
-      <UniversityCard key={university.id} university={university} showCapacity={showCapacity} />
-    ))}
-  </div>
-);
+const ITEM_HEIGHT = 101;
+
+const UniversityCards = ({ colleges, style, className, showCapacity = true }: UniversityCardsProps) => {
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const virtualizer = useVirtualizer({
+    count: colleges.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => ITEM_HEIGHT,
+    overscan: 5,
+  });
+
+  return (
+    <div
+      ref={parentRef}
+      className={clsx("h-[calc(100vh-200px)] overflow-auto", className)}
+      style={style}
+    >
+      <div
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+          width: "100%",
+          position: "relative",
+        }}
+      >
+        {virtualizer.getVirtualItems().map((virtualItem) => (
+          <div
+            key={colleges[virtualItem.index].id}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: `${virtualItem.size}px`,
+              transform: `translateY(${virtualItem.start}px)`,
+            }}
+          >
+            <div className="pb-2.5">
+              <UniversityCard university={colleges[virtualItem.index]} showCapacity={showCapacity} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default UniversityCards;
