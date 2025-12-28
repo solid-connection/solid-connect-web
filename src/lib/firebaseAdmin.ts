92625917944aaa75488b70ae621c732dac4b1f9d
manNomi <hanmw110@naver.com>
2025-12-28 19:42:08 +0900

    fix: handle undefined FIREBASE_PRIVATE_KEY in build

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -4,11 +4,12 @@ import admin from "firebase-admin";
 
 const initializeFirebaseAdmin = () => {
   if (!admin.apps.length) {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "";
     const serviceAccount = {
       type: "service_account",
       projectId: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+      privateKey,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
