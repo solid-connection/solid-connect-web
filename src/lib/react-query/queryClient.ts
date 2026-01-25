e55f12bbd8817a813420b43adc45161f889d45e6
manNomi <hanmw110@naver.com>
2026-01-25 20:51:15 +0900

    fix: queryClient에서 중복된 QueryClient import 제거

--- src/lib/react-query/queryClient.ts
+++ src/lib/react-query/queryClient.ts
@@ -1,4 +1,3 @@
-import { QueryClient } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 import { toast } from "@/lib/zustand/useToastStore";
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
