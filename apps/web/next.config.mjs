7fc0a3c992eaac731d1af23d1d44a577a86e3a81
manNomi <hanmw110@naver.com>
2026-02-09 00:49:53 +0900

    🔧 @next/bundle-analyzer 추가 및 analyze 스크립트 설정

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,10 @@
 // Injected content via Sentry wizard below
 import { withSentryConfig } from "@sentry/nextjs";
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -57,7 +62,7 @@ const nextConfig = {
 };
 
 export default withSentryConfig(
-  nextConfig,
+  withBundleAnalyzer(nextConfig),
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
