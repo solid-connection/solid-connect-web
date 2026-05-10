0608885090865f280877054682c9ab03a25b9184
yoonc01 <yoonc01@hotmail.com>
2026-05-10 23:19:12 +0900

    fix: 불필요한 h-26 삭제

--- apps/web/src/app/(home)/_ui/HomeActionLinks/index.tsx
+++ apps/web/src/app/(home)/_ui/HomeActionLinks/index.tsx
@@ -6,7 +6,7 @@ const HomeActionLinks = () => {
   return (
     <div className="flex flex-col gap-2.5 px-5 py-3.5">
       <div className="flex gap-2">
-        <Link className="h-26 flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-blue p-2.5" href="/university/search">
+        <Link className="flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-blue p-2.5" href="/university/search">
           <div className="flex flex-col">
             <span className="text-secondary typo-bold-5">학교 검색하기</span>
             <span className="text-k-700 typo-medium-4">모든 학교 목록을 확인해보세요</span>
@@ -15,7 +15,7 @@ const HomeActionLinks = () => {
             <IconMagnifyingGlass />
           </div>
         </Link>
-        <Link className="h-26 flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-sky p-2.5" href="/university/score">
+        <Link className="flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-sky p-2.5" href="/university/score">
           <div className="flex flex-col">
             <span className="text-sub-a typo-bold-5">성적 입력하기</span>
             <span className="text-k-700 typo-medium-4">성적을 입력해보세요</span>
@@ -27,7 +27,7 @@ const HomeActionLinks = () => {
       </div>
       <div className="flex gap-2">
         <Link
-          className="h-26 flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-orange p-2.5"
+          className="flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-orange p-2.5"
           href="/university/application/apply"
         >
           <div className="flex flex-col">
@@ -38,10 +38,7 @@ const HomeActionLinks = () => {
             <IconMuseum />
           </div>
         </Link>
-        <Link
-          className="h-26 flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-green p-2.5"
-          href="/university/application"
-        >
+        <Link className="flex flex-1 flex-col gap-2 rounded-lg bg-bg-accent-green p-2.5" href="/university/application">
           <div className="flex flex-col">
             <span className="text-accent-custom-green typo-bold-5">지원자 현황 확인</span>
             <span className="text-k-700 typo-medium-4">경쟁률을 바로 분석해드려요</span>
