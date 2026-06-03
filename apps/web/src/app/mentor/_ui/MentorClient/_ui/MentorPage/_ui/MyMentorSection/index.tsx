3ab42682f52ee86bb34462bd1d6afb5685cd0f24
manNomi <hanmw110@naver.com>
2026-06-03 15:12:53 +0900

    feat: 멘토 페이지 로딩 스켈레톤 적용

--- apps/web/src/app/mentor/_ui/MentorClient/_ui/MentorPage/_ui/MyMentorSection/index.tsx
+++ apps/web/src/app/mentor/_ui/MentorClient/_ui/MentorPage/_ui/MyMentorSection/index.tsx
@@ -2,9 +2,21 @@
 
 import { useGetMentorMyProfile } from "@/apis/mentor";
 import MentorCard from "@/components/mentor/MentorCard";
+import { MentorCardListSkeleton } from "../../../../../MentorPageSkeleton";
 
 const MyMentorSection = () => {
-  const { data: myMentorProfile } = useGetMentorMyProfile();
+  const { data: myMentorProfile, isPending } = useGetMentorMyProfile();
+
+  if (isPending) {
+    return (
+      <>
+        <h2 className="text-gray-900 typo-sb-5 mt-5">나의 멘토 페이지</h2>
+        <div className="mt-[14px]">
+          <MentorCardListSkeleton count={1} />
+        </div>
+      </>
+    );
+  }
 
   if (!myMentorProfile) {
     return <div className="text-gray-500">멘토 프로필을 불러오는 중...</div>;
